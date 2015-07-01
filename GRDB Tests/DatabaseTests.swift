3d8bfc04b0eb6d4c945cc61b75e5b9623605ab44
Gwendal Roué <gr@pierlis.com>
2015-07-01 08:22:10 +0200

    DatabaseQueue

--- GRDB Tests/DatabaseTests.swift
+++ GRDB Tests/DatabaseTests.swift
@@ -15,59 +15,65 @@ class DatabaseTests: XCTestCase {
     
     func testDatabase() {
         do {
-            let database = try Database(path: "/tmp/GRDB.sqlite")
+            let dbQueue = try DatabaseQueue(path: "/tmp/GRDB.sqlite")
             
-            let dropTableStmt = try database.updateStatement("DROP TABLE IF EXISTS persons")
-            try dropTableStmt.execute()
-            
-            let createTableStmt = try database.updateStatement("CREATE TABLE persons (name TEXT, age INT)")
-            try createTableStmt.execute()
-            
-            let insert1Stmt = try database.updateStatement("INSERT INTO persons (name, age) VALUES (?, ?)")
-            insert1Stmt.bind("Arthur", atIndex: 1)
-            insert1Stmt.bind(36, atIndex: 2)
-            try insert1Stmt.execute()
-            try insert1Stmt.reset()
-            insert1Stmt.bind("Arthur no age", atIndex: 1)
-            insert1Stmt.bind(nil, atIndex: 2)
-            try insert1Stmt.execute()
-            
-            let insert2Stmt = try database.updateStatement("INSERT INTO persons (name, age) VALUES (?, ?)")
-            insert2Stmt.bind(["Zoe", 65])
-            try insert2Stmt.execute()
-            try insert2Stmt.reset()
-            insert2Stmt.bind(["Zoe no age", nil])
-            try insert2Stmt.execute()
-            
-            let insert3Stmt = try database.updateStatement("INSERT INTO persons (name, age) VALUES (:name, :age)")
-            insert3Stmt.bind("Beate", forKey: ":name")
-            insert3Stmt.bind(37, forKey: ":age")
-            try insert3Stmt.execute()
-            try insert3Stmt.reset()
-            insert3Stmt.bind("Beate no age", forKey: ":name")
-            insert3Stmt.bind(nil, forKey: ":age")
-            try insert3Stmt.execute()
-            
-            let insert4Stmt = try database.updateStatement("INSERT INTO persons (name, age) VALUES (:name, :age)")
-            insert4Stmt.bind([":name": "Crystel", ":age": 16])
-            try insert4Stmt.execute()
-            try insert4Stmt.reset()
-            insert4Stmt.bind([":name": "Crystel no age", ":age": nil])
-            try insert4Stmt.execute()
-            
-            let selectStmt = try database.selectStatement("SELECT * FROM persons")
-            for row in selectStmt.fetchRows() {
-                let name = row.stringAtIndex(0)
-                let age = row.intAtIndex(1)
-                print("\(name): \(age)")
-                print("\(row.asDictionary)")
+            try dbQueue.inTransaction { db in
+                let dropTableStmt = try db.updateStatement("DROP TABLE IF EXISTS persons")
+                try dropTableStmt.execute()
+                
+                let createTableStmt = try db.updateStatement("CREATE TABLE persons (name TEXT, age INT)")
+                try createTableStmt.execute()
+                
+                let insert1Stmt = try db.updateStatement("INSERT INTO persons (name, age) VALUES (?, ?)")
+                insert1Stmt.bind("Arthur", atIndex: 1)
+                insert1Stmt.bind(36, atIndex: 2)
+                try insert1Stmt.execute()
+                try insert1Stmt.reset()
+                insert1Stmt.bind("Arthur no age", atIndex: 1)
+                insert1Stmt.bind(nil, atIndex: 2)
+                try insert1Stmt.execute()
+                
+                let insert2Stmt = try db.updateStatement("INSERT INTO persons (name, age) VALUES (?, ?)")
+                insert2Stmt.bind(["Zoe", 65])
+                try insert2Stmt.execute()
+                try insert2Stmt.reset()
+                insert2Stmt.bind(["Zoe no age", nil])
+                try insert2Stmt.execute()
+                
+                let insert3Stmt = try db.updateStatement("INSERT INTO persons (name, age) VALUES (:name, :age)")
+                insert3Stmt.bind("Beate", forKey: ":name")
+                insert3Stmt.bind(37, forKey: ":age")
+                try insert3Stmt.execute()
+                try insert3Stmt.reset()
+                insert3Stmt.bind("Beate no age", forKey: ":name")
+                insert3Stmt.bind(nil, forKey: ":age")
+                try insert3Stmt.execute()
+                
+                let insert4Stmt = try db.updateStatement("INSERT INTO persons (name, age) VALUES (:name, :age)")
+                insert4Stmt.bind([":name": "Crystel", ":age": 16])
+                try insert4Stmt.execute()
+                try insert4Stmt.reset()
+                insert4Stmt.bind([":name": "Crystel no age", ":age": nil])
+                try insert4Stmt.execute()
             }
-            for row in selectStmt.fetchRows() {
-                let name = row.stringAtIndex(0)
-                let age = row.intAtIndex(1)
-                print("\(name): \(age)")
-                print("\(row.asDictionary)")
+            
+            try dbQueue.inDatabase { db in
+                let selectStmt = try db.selectStatement("SELECT * FROM persons")
+                
+                for row in selectStmt.fetchRows() {
+                    let name = row.stringAtIndex(0)
+                    let age = row.intAtIndex(1)
+                    print("\(name): \(age)")
+                    print("\(row.asDictionary)")
+                }
+                for row in selectStmt.fetchRows() {
+                    let name = row.stringAtIndex(0)
+                    let age = row.intAtIndex(1)
+                    print("\(name): \(age)")
+                    print("\(row.asDictionary)")
+                }
             }
+            
         } catch let error as GRDB.Error {
             print("Error \(error._code), \(error.message)")
         } catch {
---
+++ GRDB/DatabaseQueue.swift
@@ -0,0 +1,49 @@
+//
+//  Queue.swift
+//  GRDB
+//
+//  Created by Gwendal Roué on 01/07/2015.
+//  Copyright © 2015 Gwendal Roué. All rights reserved.
+//
+
+public class DatabaseQueue {
+    public var database: Database { return _database! }
+    private let queue: dispatch_queue_t
+    private var _database: Database! = nil
+    
+    public init(path: String) throws {
+        queue = dispatch_queue_create("GRDB", nil)
+        _database = try Database(path: path)
+    }
+    
+    public func inDatabase(block: (db: Database) throws -> Void) throws {
+        var dbError: ErrorType?
+        dispatch_sync(queue) { () -> Void in
+            do {
+                try block(db: self.database)
+            } catch {
+                dbError = error
+            }
+        }
+        if let dbError = dbError {
+            throw dbError
+        }
+    }
+    
+    public func inTransaction(type: Database.TransactionType = .Exclusive, block: (db: Database) throws -> Void) throws {
+        var dbError: ErrorType?
+        let database = self.database
+        dispatch_sync(queue) { () -> Void in
+            do {
+                try database.inTransaction(type) { () in
+                    try block(db: database)
+                }
+            } catch {
+                dbError = error
+            }
+        }
+        if let dbError = dbError {
+            throw dbError
+        }
+    }
+}
