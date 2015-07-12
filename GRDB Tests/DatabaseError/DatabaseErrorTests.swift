b90382f3c5d11b0abccee45acf784b422008e0c4
Gwendal Roué <gr@pierlis.com>
2015-07-12 08:25:48 +0200

    Add test for rollbacked transactions

--- GRDB Tests/DatabaseError/DatabaseErrorTests.swift
+++ GRDB Tests/DatabaseError/DatabaseErrorTests.swift
@@ -43,6 +43,29 @@ class DatabaseErrorTests: GRDBTestCase {
         }
     }
     
+    func testDatabaseErrorInTransaction() {
+        do {
+            try dbQueue.inTransaction { db in
+                try db.execute("CREATE TABLE persons (id INTEGER PRIMARY KEY)")
+                try db.execute("CREATE TABLE pets (masterId INTEGER NOT NULL REFERENCES persons(id), name TEXT)")
+                self.sqlQueries.removeAll()
+                try db.execute("INSERT INTO pets (masterId, name) VALUES (?, ?)", bindings: [1, "Bobby"])
+                return .Commit
+            }
+        } catch let error as DatabaseError {
+            XCTAssertEqual(error.code, Int(SQLITE_CONSTRAINT))
+            XCTAssertEqual(error.message!, "FOREIGN KEY constraint failed")
+            XCTAssertEqual(error.sql!, "INSERT INTO pets (masterId, name) VALUES (?, ?)")
+            XCTAssertEqual(error.description, "SQLite error 19 with statement `INSERT INTO pets (masterId, name) VALUES (?, ?)` bindings [1, \"Bobby\"]: FOREIGN KEY constraint failed")
+            
+            XCTAssertEqual(sqlQueries.count, 2)
+            XCTAssertEqual(sqlQueries[0], "INSERT INTO pets (masterId, name) VALUES (?, ?)")
+            XCTAssertEqual(sqlQueries[1], "ROLLBACK TRANSACTION")
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+    
     func testDatabaseErrorThrownByUpdateStatementContainSQLAndBindings() {
         dbQueue.inDatabase { db in
             do {
