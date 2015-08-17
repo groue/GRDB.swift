332eab7cd8c1764c8017d36d7ce5dc1b53445be7
Gwendal Roué <gr@pierlis.com>
2015-08-17 12:42:23 +0200

    Update tests for Xcode7b5

--- GRDB Tests/Public/RowModel/RowModelDescriptionTests.swift
+++ GRDB Tests/Public/RowModel/RowModelDescriptionTests.swift
@@ -49,20 +49,20 @@ class RowModelDescriptionTests: RowModelTestCase {
 
     func testEmptyRowModelDescription() {
         let model = EmptyRowModel()
-        XCTAssertEqual(model.description, "<GRDBTests.EmptyRowModel>")
+        XCTAssertEqual(model.description, "<EmptyRowModel>")
     }
     
     func testSimpleRowModelDescription() {
         let model = SingleColumnRowModel()
         model.name = "foo"
-        XCTAssertEqual(model.description, "<GRDBTests.SingleColumnRowModel name:\"foo\">")
+        XCTAssertEqual(model.description, "<SingleColumnRowModel name:\"foo\">")
     }
     
     func testDoubleColumnRowModelDescription() {
         let model = DoubleColumnRowModel()
         model.name = "foo"
         model.age = 35
-        XCTAssertTrue(["<GRDBTests.DoubleColumnRowModel name:\"foo\" age:35>", "<GRDBTests.DoubleColumnRowModel age:35 name:\"foo\">"].indexOf(model.description) != nil)
+        XCTAssertTrue(["<DoubleColumnRowModel name:\"foo\" age:35>", "<DoubleColumnRowModel age:35 name:\"foo\">"].indexOf(model.description) != nil)
     }
 
 }
