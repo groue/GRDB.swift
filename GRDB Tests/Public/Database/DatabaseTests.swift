a83aad93ffda6bffd15974cce2b7eba8025e036b
Gwendal Roué <gr@pierlis.com>
2015-08-17 17:03:04 +0200

    #4 WIP: Failing test that Database.execute() with a single SQL statement and no argument successfully returns the inserted Row ID.

--- GRDB Tests/Public/Database/DatabaseTests.swift
+++ GRDB Tests/Public/Database/DatabaseTests.swift
@@ -116,6 +116,26 @@ class DatabaseTests : GRDBTestCase {
         }
     }
     
+    func testChangesReturnedByDatabaseExecute() {
+        assertNoError {
+            try dbQueue.inDatabase { db in
+                try db.execute("CREATE TABLE persons (id INTEGER PRIMARY KEY, name TEXT)")
+                
+                let changes1 = try db.execute("INSERT INTO persons (name) VALUES ('Arthur')")
+                XCTAssertEqual(changes1.changedRowCount, 1)
+                XCTAssertEqual(changes1.insertedRowID, 1)
+                
+                let changes2 = try db.execute("INSERT INTO persons (name) VALUES (?)", arguments: ["Barbara"])
+                XCTAssertEqual(changes2.changedRowCount, 1)
+                XCTAssertEqual(changes2.insertedRowID, 2)
+                
+                let changes3 = try db.execute("DELETE FROM persons")
+                XCTAssertEqual(changes3.changedRowCount, 2)
+                XCTAssertTrue(changes3.insertedRowID == nil)
+            }
+        }
+    }
+    
     func testDatabaseExecuteWithArrayBinding() {
         assertNoError {
             try dbQueue.inDatabase { db in
