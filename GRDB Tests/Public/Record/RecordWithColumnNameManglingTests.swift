9aa68235e6184c4f068832bf01d1ef0403ab82aa
Gwendal Roué <gr@pierlis.com>
2015-09-11 12:44:03 +0200

    Missing support for column name mangling: cleanup

--- GRDB Tests/Public/Record/RecordWithColumnNameManglingTests.swift
+++ GRDB Tests/Public/Record/RecordWithColumnNameManglingTests.swift
@@ -1,90 +1,56 @@
 import XCTest
 import GRDB
 
-class WrongMangling : Record {
+// BadlyMangledStuff.updateFromRow() accepts a row with mangled column names.
+// Its databaseEdited flag is wrong.
+class BadlyMangledStuff : Record {
     var id: Int64?
     var name: String?
     
     override static func databaseTableName() -> String {
-        return "mangles"
+        return "stuffs"
     }
     
     override var storedDatabaseDictionary: [String: DatabaseValueConvertible?] {
-        // User won't peek fancy column names, here, because he will notice that
-        // the generated INSERT query uses exactly those names:
+        // User won't peek fancy column names because he will notice that the
+        // generated INSERT query needs actual column names.
         return ["id": id, "name": name]
     }
     
     override func updateFromRow(row: Row) {
-        // Here user may peed fancy column names that match his SQL queries.
-        // However this is not the way to do it.
+        // Here user may peek fancy column names that match his SQL queries.
+        // However this is not the way to do it (see testBadlyMangledStuff()).
         if let dbv = row["mangled_id"] { id = dbv.value() }
         if let dbv = row["mangled_name"] { name = dbv.value() }
         super.updateFromRow(row)
     }
 }
 
-class GoodMangling : Record {
-    var id: Int64?
-    var name: String?
-    
-    override static func databaseTableName() -> String {
-        return "mangles"
-    }
-    
-    override var storedDatabaseDictionary: [String: DatabaseValueConvertible?] {
-        // Regular column names
-        return ["id": id, "name": name]
-    }
-    
-    override func updateFromRow(row: Row) {
-        // Regular column names
-        if let dbv = row["id"] { id = dbv.value() }
-        if let dbv = row["name"] { name = dbv.value() }
-        super.updateFromRow(row)
-    }
-}
-
 class RecordWithColumnNameManglingTests: GRDBTestCase {
     
-    func testWrongMangling() {
+    func testBadlyMangledStuff() {
         assertNoError {
             try dbQueue.inDatabase { db in
-                try db.execute("CREATE TABLE mangles (id INTEGER PRIMARY KEY, name TEXT)")
+                try db.execute("CREATE TABLE stuffs (id INTEGER PRIMARY KEY, name TEXT)")
                 do {
-                    let record = WrongMangling()
+                    let record = BadlyMangledStuff()
                     record.name = "foo"
                     try record.save(db)
+                    
+                    // Nothing special here
                     XCTAssertFalse(record.databaseEdited)
                 }
                 do {
-                    let record = WrongMangling.fetchOne(db, "SELECT id AS mangled_id, name AS mangled_name FROM mangles")!
+                    let record = BadlyMangledStuff.fetchOne(db, "SELECT id AS mangled_id, name AS mangled_name FROM stuffs")!
+                    // OK we could extract values.
                     XCTAssertEqual(record.id, 1)
                     XCTAssertEqual(record.name, "foo")
-                    XCTAssertTrue(record.databaseEdited)    // Here lies the problem with WrongMangling.
+                    
+                    // But here lies the problem with BadlyMangledStuff.
+                    // It should not be edited:
+                    XCTAssertTrue(record.databaseEdited)
                 }
             }
         }
     }
-    
-    func testGoodMangling() {
-        assertNoError {
-            try dbQueue.inDatabase { db in
-                try db.execute("CREATE TABLE mangles (id INTEGER PRIMARY KEY, name TEXT)")
-                do {
-                    let record = GoodMangling()
-                    record.name = "foo"
-                    try record.save(db)
-                    XCTAssertFalse(record.databaseEdited)
-                }
-                do {
-                    let record = GoodMangling.fetchOne(db, "SELECT id AS mangled_id, name AS mangled_name FROM mangles")!
-                    XCTAssertEqual(record.id, 1)
-                    XCTAssertEqual(record.name, "foo")
-                    XCTAssertFalse(record.databaseEdited)    // GoodMangling does it better.
-                }
-            }
-        }
-    }
-    
 }
