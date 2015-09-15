fd728760ed5bdc3272cc2bd7160d7dfd87ecf98c
Gwendal Roué <gr@pierlis.com>
2015-09-15 19:44:51 +0200

    Profile with GRDBProfiling project.

---
+++ GRDB Tests/GRDBProfiling/GRDBProfiling/AppDelegate.swift
@@ -0,0 +1,59 @@
+//
+//  AppDelegate.swift
+//  GRDBProfiling
+//
+//  Created by Gwendal Roué on 15/09/2015.
+//  Copyright © 2015 Gwendal Roué. All rights reserved.
+//
+
+import Cocoa
+import GRDB
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var dbQueue: DatabaseQueue!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+        let databasePath = "/tmp/GRDBProfiling.sqlite"
+        do { try NSFileManager.defaultManager().removeItemAtPath(databasePath) } catch { }
+        dbQueue = try! DatabaseQueue(path: databasePath)
+        populateDatabase()
+        readFromDatabase()
+    }
+    
+    func populateDatabase() {
+        try! dbQueue.inTransaction { db in
+            try db.execute(
+                "CREATE TABLE items (" +
+                    "i0 INT, " +
+                    "i1 INT, " +
+                    "i2 INT, " +
+                    "i3 INT, " +
+                    "i4 INT " +
+                ")")
+            for i in 0..<10000 {
+                try db.execute("INSERT INTO items (i0, i1, i2, i3, i4) VALUES (?,?,?,?,?)", arguments: [i, i+1, i+2, i+3, i+4])
+            }
+            return .Commit
+        }
+    }
+    
+    func readFromDatabase() {
+        for _ in 0..<100 {
+            var sum: Int64 = 0
+            dbQueue.inDatabase { db in
+                for row in Row.fetch(db, "SELECT * FROM items") {
+                    let i0: Int64 = row.value(atIndex: 0)!
+                    let i1: Int64 = row.value(atIndex: 1)!
+                    let i2: Int64 = row.value(atIndex: 2)!
+                    let i3: Int64 = row.value(atIndex: 3)!
+                    let i4: Int64 = row.value(atIndex: 4)!
+                    sum += i0 + i1 + i2 + i3 + i4
+                }
+            }
+            print(sum)
+        }
+    }
+}
+
---
+++ GRDB Tests/GRDBProfiling/GRDBProfiling/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GRDBProfiling
+//
+//  Created by Gwendal Roué on 15/09/2015.
+//  Copyright © 2015 Gwendal Roué. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
