3516036bbc4a4b86f38d64a7cd6cca586a6d0171
Gwendal Roué <gr@pierlis.com>
2015-10-27 10:32:18 +0100

    Code cleanup

--- GRDB/Core/Protocols/DatabaseTableMapping.swift
+++ GRDB/Core/Protocols/DatabaseTableMapping.swift
@@ -14,48 +14,6 @@ public protocol DatabaseTableMapping : RowConvertible {
 
 extension DatabaseTableMapping {
     
-    // Returns "SELECT * FROM table WHERE id IN (?,?,?)"
-    //
-    // Returns nil if primaryKeys is empty.
-    private static func fetchByPrimaryKeyStatement<Sequence: SequenceType where Sequence.Generator.Element: DatabaseValueConvertible>(db: Database, primaryKeys: Sequence) -> SelectStatement? {
-        // Fail early if databaseTable is nil
-        guard let databaseTableName = self.databaseTableName() else {
-            fatalError("Nil returned from \(self).databaseTableName()")
-        }
-        
-        // Fail early if database table does not exist.
-        guard let primaryKey = db.primaryKeyForTable(named: databaseTableName) else {
-            fatalError("Table \(databaseTableName.quotedDatabaseIdentifier) does not exist. See \(self).databaseTableName()")
-        }
-        
-        // Fail early if database table has not one column in its primary key
-        let columns = primaryKey.columns
-        guard columns.count == 1 else {
-            fatalError("Primary key of table \(databaseTableName.quotedDatabaseIdentifier) is not made of a single column. See \(self).databaseTableName()")
-        }
-        
-        let primaryKeys = primaryKeys.map { $0 as DatabaseValueConvertible? }
-        
-        switch primaryKeys.count {
-        case 0:
-            // Avoid performing useless SELECT
-            return nil
-        case 1:
-            // Use '=' in SQL query
-            let sql = "SELECT * FROM \(databaseTableName.quotedDatabaseIdentifier) WHERE \(columns.first!.quotedDatabaseIdentifier) = ?"
-            let statement = db.selectStatement(sql)
-            statement.arguments = StatementArguments(primaryKeys)
-            return statement
-        default:
-            // Use 'IN'
-            let questionMarks = Array(count: primaryKeys.count, repeatedValue: "?").joinWithSeparator(",")
-            let sql = "SELECT * FROM \(databaseTableName.quotedDatabaseIdentifier) WHERE \(columns.first!.quotedDatabaseIdentifier) IN (\(questionMarks))"
-            let statement = db.selectStatement(sql)
-            statement.arguments = StatementArguments(primaryKeys)
-            return statement
-        }
-    }
-    
     /// Fetches a sequence of values, given their primary keys.
     ///
     ///     let persons = Person.fetch(db, primaryKeys: [1, 2, 3]) // DatabaseSequence<Person>
@@ -101,11 +59,7 @@ extension DatabaseTableMapping {
         guard let primaryKey = primaryKey else {
             return nil
         }
-        if let statement = self.fetchByPrimaryKeyStatement(db, primaryKeys: [primaryKey]) {
-            return self.fetchOne(statement)
-        } else {
-            return nil
-        }
+        return self.fetchOne(self.fetchByPrimaryKeyStatement(db, primaryKeys: [primaryKey])!)
     }
     
     /// Fetches a single value given a key.
@@ -129,5 +83,47 @@ extension DatabaseTableMapping {
         let whereSQL = dictionary.keys.map { column in "\(column.quotedDatabaseIdentifier)=?" }.joinWithSeparator(" AND ")
         let sql = "SELECT * FROM \(databaseTableName.quotedDatabaseIdentifier) WHERE \(whereSQL)"
         return fetchOne(db.selectStatement(sql), arguments: StatementArguments(dictionary.values))
+    }
+    
+    // Returns "SELECT * FROM table WHERE id IN (?,?,?)"
+    //
+    // Returns nil if primaryKeys is empty.
+    private static func fetchByPrimaryKeyStatement<Sequence: SequenceType where Sequence.Generator.Element: DatabaseValueConvertible>(db: Database, primaryKeys: Sequence) -> SelectStatement? {
+        // Fail early if databaseTable is nil
+        guard let databaseTableName = self.databaseTableName() else {
+            fatalError("Nil returned from \(self).databaseTableName()")
+        }
+        
+        // Fail early if database table does not exist.
+        guard let primaryKey = db.primaryKeyForTable(named: databaseTableName) else {
+            fatalError("Table \(databaseTableName.quotedDatabaseIdentifier) does not exist. See \(self).databaseTableName()")
+        }
+        
+        // Fail early if database table has not one column in its primary key
+        let columns = primaryKey.columns
+        guard columns.count == 1 else {
+            fatalError("Primary key of table \(databaseTableName.quotedDatabaseIdentifier) is not made of a single column. See \(self).databaseTableName()")
+        }
+        
+        let primaryKeys = primaryKeys.map { $0 as DatabaseValueConvertible? }
+        
+        switch primaryKeys.count {
+        case 0:
+            // Avoid performing useless SELECT
+            return nil
+        case 1:
+            // Use '=' in SQL query
+            let sql = "SELECT * FROM \(databaseTableName.quotedDatabaseIdentifier) WHERE \(columns.first!.quotedDatabaseIdentifier) = ?"
+            let statement = db.selectStatement(sql)
+            statement.arguments = StatementArguments(primaryKeys)
+            return statement
+        default:
+            // Use 'IN'
+            let questionMarks = Array(count: primaryKeys.count, repeatedValue: "?").joinWithSeparator(",")
+            let sql = "SELECT * FROM \(databaseTableName.quotedDatabaseIdentifier) WHERE \(columns.first!.quotedDatabaseIdentifier) IN (\(questionMarks))"
+            let statement = db.selectStatement(sql)
+            statement.arguments = StatementArguments(primaryKeys)
+            return statement
+        }
     }
 }
