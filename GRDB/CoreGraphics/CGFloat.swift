85750594265ea57af9d719a4de6dc34f236d77cf
Gwendal Roué <gr@pierlis.com>
2015-10-29 07:51:59 +0100

    CGFloat documentation

--- GRDB/CoreGraphics/CGFloat.swift
+++ GRDB/CoreGraphics/CGFloat.swift
@@ -3,10 +3,15 @@ import CoreGraphics
 /// CGFloat adopts DatabaseValueConvertible
 extension CGFloat : DatabaseValueConvertible {
 
+    /// Returns a value that can be stored in the database.
     public var databaseValue: DatabaseValue {
         return DatabaseValue(double: Double(self))
     }
     
+    /// Returns an instance initialized from *databaseValue*, if possible.
+    ///
+    /// - parameter databaseValue: A DatabaseValue.
+    /// - returns: An optional CGFloat.
     public static func fromDatabaseValue(databaseValue: DatabaseValue) -> CGFloat? {
         guard let double = Double.fromDatabaseValue(databaseValue) else {
             return nil
