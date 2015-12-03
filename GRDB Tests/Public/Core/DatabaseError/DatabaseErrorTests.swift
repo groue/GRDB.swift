20734d51623177b693f3f52ad4c80f08c0c0c65d
Gwendal Roué <gr@pierlis.com>
2015-12-03 07:56:33 +0100

    Tests that errors thrown by UpdateStatement contain the correct arguments

--- GRDB Tests/Public/Core/DatabaseError/DatabaseErrorTests.swift
+++ GRDB Tests/Public/Core/DatabaseError/DatabaseErrorTests.swift
@@ -27,11 +27,50 @@ class DatabaseErrorTests: GRDBTestCase {
     }
     
     func testDatabaseErrorThrownByUpdateStatementContainSQLAndArguments() {
-        dbQueue.inDatabase { db in
-            do {
+        assertNoError {
+            try dbQueue.inDatabase { db in
                 try db.execute("CREATE TABLE persons (id INTEGER PRIMARY KEY)")
                 try db.execute("CREATE TABLE pets (masterId INTEGER NOT NULL REFERENCES persons(id), name TEXT)")
+            }
+        }
+        
+        // db.execute(sql, arguments)
+        dbQueue.inDatabase { db in
+            do {
                 try db.execute("INSERT INTO pets (masterId, name) VALUES (?, ?)", arguments: [1, "Bobby"])
+                XCTFail()
+            } catch let error as DatabaseError {
+                XCTAssertEqual(error.code, 19) // SQLITE_CONSTRAINT
+                XCTAssertEqual(error.message!, "FOREIGN KEY constraint failed")
+                XCTAssertEqual(error.sql!, "INSERT INTO pets (masterId, name) VALUES (?, ?)")
+                XCTAssertEqual(error.description, "SQLite error 19 with statement `INSERT INTO pets (masterId, name) VALUES (?, ?)` arguments [1, \"Bobby\"]: FOREIGN KEY constraint failed")
+            } catch {
+                XCTFail("\(error)")
+            }
+        }
+
+        // statement.execute(arguments)
+        dbQueue.inDatabase { db in
+            do {
+                let statement = db.updateStatement("INSERT INTO pets (masterId, name) VALUES (?, ?)")
+                try statement.execute(arguments: [1, "Bobby"])
+                XCTFail()
+            } catch let error as DatabaseError {
+                XCTAssertEqual(error.code, 19) // SQLITE_CONSTRAINT
+                XCTAssertEqual(error.message!, "FOREIGN KEY constraint failed")
+                XCTAssertEqual(error.sql!, "INSERT INTO pets (masterId, name) VALUES (?, ?)")
+                XCTAssertEqual(error.description, "SQLite error 19 with statement `INSERT INTO pets (masterId, name) VALUES (?, ?)` arguments [1, \"Bobby\"]: FOREIGN KEY constraint failed")
+            } catch {
+                XCTFail("\(error)")
+            }
+        }
+        
+        // statement.execute()
+        dbQueue.inDatabase { db in
+            do {
+                let statement = db.updateStatement("INSERT INTO pets (masterId, name) VALUES (?, ?)")
+                statement.arguments = [1, "Bobby"]
+                try statement.execute()
                 XCTFail()
             } catch let error as DatabaseError {
                 XCTAssertEqual(error.code, 19) // SQLITE_CONSTRAINT
