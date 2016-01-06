7bd21f499ec52642f605dfcfe6d4afc2f2d4c470
Gwendal Roué <gr@pierlis.com>
2016-01-06 08:54:09 +0100

    Project organization

--- GRDB/Core/Statement.swift
+++ GRDB/Core/Statement.swift
@@ -354,6 +354,20 @@ public final class UpdateStatement : Statement {
     }
 }
 
+/// Represents the various changes made to the database via execution of one or
+/// more SQL statements.
+public struct DatabaseChanges {
+    
+    /// The number of rows affected by the statement(s)
+    public let changedRowCount: Int
+    
+    /// The inserted Row ID.
+    ///
+    /// This value is only relevant after the execution of a single INSERT
+    /// statement, via Database.execute() or UpdateStatement.execute().
+    public let insertedRowID: Int64?
+}
+
 
 // MARK: - StatementArguments
 
