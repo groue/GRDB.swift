6de0debb49c0d30b37e21aa6eca0e3fcfc9d8597
Gwendal Roué <gr@pierlis.com>
2016-05-13 11:35:38 +0200

    More RowAdapter tests

--- Tests/Public/Core/Row/RowAdapterTests.swift
+++ Tests/Public/Core/Row/RowAdapterTests.swift
@@ -11,14 +11,15 @@ class RowAdapterTests: GRDBTestCase {
                 subrows: [
                     "foo": ["id": "barid", "val": "barval"],
                     "odd": ["id": "missingid"]])
-            let row = Row.fetchOne(db, "SELECT 1 AS fooid, 'toto' AS fooval, 2 as barid, 'tata' AS barval", adapter: adapter)!
+            let row = Row.fetchOne(db, "SELECT 1 AS fooid, 'foo' AS fooval, 2 as barid, 'bar' AS barval", adapter: adapter)!
             
-            print(row)                  // <Row id:1 val:"toto">
+            // # Row values
+            
             XCTAssertEqual(row.count, 2)
             XCTAssertEqual(row.value(named: "id") as Int, 1)
-            XCTAssertEqual(row.value(named: "val") as String, "toto")
+            XCTAssertEqual(row.value(named: "val") as String, "foo")
             XCTAssertEqual(row.databaseValue(named: "id"), 1.databaseValue)
-            XCTAssertEqual(row.databaseValue(named: "val"), "toto".databaseValue)
+            XCTAssertEqual(row.databaseValue(named: "val"), "foo".databaseValue)
             XCTAssertTrue(row.value(named: "barid") == nil)
             XCTAssertTrue(row.value(named: "missing") == nil)
             XCTAssertTrue(row.hasColumn("id"))
@@ -28,10 +29,23 @@ class RowAdapterTests: GRDBTestCase {
             // TODO: test row.value(atIndex: 0) and row.value(atIndex: 1)
             // TODO: test for (key, value) in row { ... }
             
+            
+            // # Row equality
+            
+            // One of this tests is failing - can't know which one because dictionaries are unordered.
+            // TODO: think about that.
+            let altRow1 = Row.fetchOne(db, "SELECT 1 AS id, 'foo' AS val")!
+            XCTAssertEqual(row, altRow1)
+            let altRow2 = Row.fetchOne(db, "SELECT 'foo' AS val, 1 AS id")!
+            XCTAssertEqual(row, altRow2)
+            
+            
+            // # Subrows
+            
             let row2 = row.subrows["foo"]!
             XCTAssertEqual(row2.count, 2)
             XCTAssertEqual(row2.value(named: "id") as Int, 2)
-            XCTAssertEqual(row2.value(named: "val") as String, "tata")
+            XCTAssertEqual(row2.value(named: "val") as String, "bar")
             
             let row3 = row.subrows["odd"]!
             XCTAssertEqual(row3.count, 1)
