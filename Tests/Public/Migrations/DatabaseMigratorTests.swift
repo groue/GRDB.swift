37d7c178f5a2854b211c5d83061c23736487a5c4
Gwendal Roué <gr@pierlis.com>
2016-05-17 19:43:15 +0200

    DatabaseMigratorTests: accept various error messages from various SQLite versions

--- Tests/Public/Migrations/DatabaseMigratorTests.swift
+++ Tests/Public/Migrations/DatabaseMigratorTests.swift
@@ -117,9 +117,9 @@ class DatabaseMigratorTests : GRDBTestCase {
                 // The second migration should be rollbacked.
                 
                 XCTAssertEqual(error.code, 19) // SQLITE_CONSTRAINT
-                XCTAssertEqual(error.message!, "FOREIGN KEY constraint failed")
+                XCTAssertEqual(error.message!.lowercaseString, "foreign key constraint failed") // lowercaseString: accept multiple SQLite version
                 XCTAssertEqual(error.sql!, "INSERT INTO pets (masterId, name) VALUES (?, ?)")
-                XCTAssertEqual(error.description, "SQLite error 19 with statement `INSERT INTO pets (masterId, name) VALUES (?, ?)` arguments [123, \"Bobby\"]: FOREIGN KEY constraint failed")
+                XCTAssertEqual(error.description.lowercaseString, "sqlite error 19 with statement `insert into pets (masterid, name) values (?, ?)` arguments [123, \"bobby\"]: foreign key constraint failed")
 
                 let names = dbQueue.inDatabase { db in
                     String.fetchAll(db, "SELECT name FROM persons")
