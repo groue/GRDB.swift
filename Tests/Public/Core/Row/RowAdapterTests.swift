23178a2727e5976794aed18beae6b40a5763e5b6
Gwendal Roué <gr@pierlis.com>
2016-05-18 09:09:09 +0200

    Issue #50: More RowAdapterTests

--- Tests/Public/Core/Row/RowAdapterTests.swift
+++ Tests/Public/Core/Row/RowAdapterTests.swift
@@ -7,30 +7,36 @@ class RowAdapterTests: GRDBTestCase {
         let dbQueue = DatabaseQueue()
         dbQueue.inDatabase { db in
             let adapter = RowAdapter(
-                // Main mapping
-                mapping: ["id": "fooid", "val": "fooval"],
+                mapping: ["id": "fooid", "val": "FOOVAL"],   // case insensitivity of base column names
                 subrowMappings: [
-                    // Alternate mapping
-                    "foo": ["id": "BARID", "val": "BARVAL"]])
+                    "foo": ["id": "barid", "val": "barval"]])
             let row = Row.fetchOne(db, "SELECT 1 AS fooid, 'foo' AS fooval, 2 as barid, 'bar' AS barval", adapter: adapter)!
             
-            // # Row values
             
-            XCTAssertEqual(row.count, 2)
-            XCTAssertEqual(row.value(atIndex: 0) as Int, 1)
-            XCTAssertEqual(row.value(atIndex: 1) as String, "foo")
+            // # Column names
+            
             XCTAssertEqual(row.value(named: "id") as Int, 1)
+            XCTAssertEqual(row.value(named: "ID") as Int, 1)    // case insensitivity of mapped column names
             XCTAssertEqual(row.value(named: "val") as String, "foo")
-            XCTAssertEqual(row.databaseValue(atIndex: 0), 1.databaseValue)
-            XCTAssertEqual(row.databaseValue(atIndex: 1), "foo".databaseValue)
             XCTAssertEqual(row.databaseValue(named: "id"), 1.databaseValue)
+            XCTAssertEqual(row.databaseValue(named: "ID"), 1.databaseValue) // case insensitivity of mapped column names
             XCTAssertEqual(row.databaseValue(named: "val"), "foo".databaseValue)
             XCTAssertTrue(row.value(named: "barid") == nil)
             XCTAssertTrue(row.value(named: "missing") == nil)
             XCTAssertTrue(row.hasColumn("id"))
+            XCTAssertTrue(row.hasColumn("ID")) // case insensitivity of mapped column names
             XCTAssertTrue(row.hasColumn("val"))
             XCTAssertFalse(row.hasColumn("barid"))
             XCTAssertFalse(row.hasColumn("missing"))
+            
+            
+            // # Column ordering
+            
+            XCTAssertEqual(row.value(atIndex: 0) as Int, 1)
+            XCTAssertEqual(row.value(atIndex: 1) as String, "foo")
+            XCTAssertEqual(row.databaseValue(atIndex: 0), 1.databaseValue)
+            XCTAssertEqual(row.databaseValue(atIndex: 1), "foo".databaseValue)
+            XCTAssertEqual(row.count, 2)
             XCTAssertEqual(Array(row.columnNames), ["id", "val"])
             XCTAssertEqual(Array(row.databaseValues), [1.databaseValue, "foo".databaseValue])
             let pairs = row.map { (columnName: $0, databaseValue: $1) }
