49b35a92166d579f831a3744cfc2d022f56c99a7
Gwendal Roué <gr@pierlis.com>
2016-05-19 09:05:12 +0200

    More failing tests for issue #53

--- Tests/Public/Core/DatabaseQueue/DatabaseQueueTests.swift
+++ Tests/Public/Core/DatabaseQueue/DatabaseQueueTests.swift
@@ -6,40 +6,64 @@ class DatabaseQueueTests: GRDBTestCase {
     func testSwiftCompiler() {
         // Here we test that Swift compiler compiles some various usages of
         // DatabaseQueue.inDatabase { ... }
-        let dbQueue = DatabaseQueue()
         
         assertNoError {
+            let dbQueue = DatabaseQueue()
             try dbQueue.inDatabase { db in
                 try db.execute("CREATE TABLE foo (id INTEGER PRIMARY KEY")
             }
         }
         
         assertNoError {
+            let dbQueue = DatabaseQueue()
             try dbQueue.inDatabase { db in
                 try db.execute("CREATE TABLE foo (id INTEGER PRIMARY KEY")
                 try db.execute("CREATE TABLE bar (id INTEGER PRIMARY KEY")
             }
         }
         
+        assertNoError {
+            let dbQueue = DatabaseQueue()
+            // TODO: make it compile
+            try dbQueue.inDatabase { db in
+                try db.execute("CREATE TABLE foo (id INTEGER PRIMARY KEY")
+                return 1
+            }
+        }
+        
+        assertNoError {
+            let dbQueue = DatabaseQueue()
+            // TODO: make it compile
+            let x = try dbQueue.inDatabase { db in
+                try db.execute("CREATE TABLE foo (id INTEGER PRIMARY KEY")
+                return 1
+            }
+        }
+        
         do {
+            let dbQueue = DatabaseQueue()
             dbQueue.inDatabase { db in
                 let x = 1
             }
         }
         
         do {
+            let dbQueue = DatabaseQueue()
             let x = dbQueue.inDatabase { db in
                 1
             }
         }
         
         do {
+            let dbQueue = DatabaseQueue()
             dbQueue.inDatabase { db in
                 1
             }
         }
         
         do {
+            let dbQueue = DatabaseQueue()
+            // TODO: make it compile
             let x = dbQueue.inDatabase { db in
                 let a = 1
                 let b = 2
@@ -48,6 +72,7 @@ class DatabaseQueueTests: GRDBTestCase {
         }
         
         do {
+            // TODO: make it compile
             dbQueue.inDatabase { db in
                 let a = 1
                 let b = 2
