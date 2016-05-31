cb8331f21355da083c60cec0a9b3b98c2ab25213
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-05-30 21:16:36 -0400

    Renamed customizable files to <file>-USER, added .example files, preserve user customizations.
    Added .example files.
    Added build phase script to copy .example files if user configuration
    files are not already present.

--- SQLiteCustom/GRDBCustomSQLite.h
+++ SQLiteCustom/GRDBCustomSQLite.h
@@ -6,8 +6,6 @@ FOUNDATION_EXPORT double GRDB_VersionNumber;
 //! Project version string for GRDB.
 FOUNDATION_EXPORT const unsigned char GRDB_VersionString[];
 
-// INSERT ANY ADDITIONAL SQLITE DEFINES HERE
-#define SQLITE_ENABLE_PREUPDATE_HOOK
-// -----------------------------------------
+#import <GRDBCustomSQLite/GRDBCustomSQLite-USER.h>
 #import <GRDBCustomSQLite/GRDBCustomSQLite-Bridging.h>
 #import <GRDBCustomSQLite/sqlite3.h>
