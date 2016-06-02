b24631345bf021d3ed55be44f43060b46f18b166
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-06-02 18:08:28 -0400

    Increase test coverage of NSData.swift

--- Tests/Public/Core/DatabaseValueConvertible/NSDataTests.swift
+++ Tests/Public/Core/DatabaseValueConvertible/NSDataTests.swift
@@ -14,4 +14,31 @@ class NSDataTests: GRDBTestCase {
         let databaseValue = NSData().databaseValue
         XCTAssertEqual(databaseValue, DatabaseValue.Null)
     }
+    
+    func testNSDataDatabaseValueRoundTrip() {
+        
+        func roundTrip(value: NSData) -> Bool
+        {
+            let databaseValue = value.databaseValue
+            guard let back = NSData.fromDatabaseValue(databaseValue) else
+            {
+                XCTFail("Failed to convert from DatabaseValue to NSData")
+                return false
+            }
+            return back.isEqual(value)
+        }
+        
+        XCTAssertTrue(roundTrip("bar".dataUsingEncoding(NSUTF8StringEncoding)!))
+    }
+    
+    func testNSDataFromDatabaseValueFailure() {
+        let databaseValue_Null = DatabaseValue.Null
+        let databaseValue_Int64 = Int64(1).databaseValue
+        let databaseValue_Double = Double(100000.1).databaseValue
+        let databaseValue_String = "foo".databaseValue
+        XCTAssertNil(NSData.fromDatabaseValue(databaseValue_Null))
+        XCTAssertNil(NSData.fromDatabaseValue(databaseValue_Int64))
+        XCTAssertNil(NSData.fromDatabaseValue(databaseValue_Double))
+        XCTAssertNil(NSData.fromDatabaseValue(databaseValue_String))
+    }
 }
