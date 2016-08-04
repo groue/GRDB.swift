8332dc188a87617035520700e0d233f25de08723
Gwendal Roué <gr@pierlis.com>
2016-08-04 13:34:14 +0200

    SQLTableBuilder tests

--- Tests/Public/SQLTableBuilder/SQLTableBuilderTests.swift
+++ Tests/Public/SQLTableBuilder/SQLTableBuilderTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 class SQLTableBuilderTests: GRDBTestCase {
 
-    func testSQLTableBuilder() {
+    func testCreateTable() {
         assertNoError {
             let dbQueue = try makeDatabaseQueue()
             try dbQueue.inDatabase { db in
@@ -18,17 +18,69 @@ class SQLTableBuilderTests: GRDBTestCase {
                     t.column("id", .Integer).primaryKey()
                     t.column("name", .Text)
                 }
-                XCTAssertEqual(self.lastSQLQuery, "CREATE TABLE \"test\" (\"id\" INTEGER PRIMARY KEY, \"name\" TEXT)")
-                
-                // Drop table
-                try db.drop(table: "test")
-                XCTAssertEqual(self.lastSQLQuery, "DROP TABLE \"test\"")
-                
-                // Table options and column primary key options
+                XCTAssertEqual(self.lastSQLQuery,
+                    "CREATE TABLE \"test\" (" +
+                        "\"id\" INTEGER PRIMARY KEY, " +
+                        "\"name\" TEXT" +
+                    ")")
+            }
+        }
+    }
+    
+    func testTableCreationOptions() {
+        assertNoError {
+            let dbQueue = try makeDatabaseQueue()
+            try dbQueue.inDatabase { db in
                 try db.create(table: "test", temporary: true, ifNotExists: true, withoutRowID: true) { t in
+                    t.column("id", .Integer).primaryKey()
+                }
+                XCTAssertEqual(self.lastSQLQuery,
+                    "CREATE TEMPORARY TABLE IF NOT EXISTS \"test\" (" +
+                        "\"id\" INTEGER PRIMARY KEY" +
+                    ") WITHOUT ROWID")
+            }
+        }
+    }
+    
+    func testColumnPrimaryKeyOptions() {
+        assertNoError {
+            let dbQueue = try makeDatabaseQueue()
+            try dbQueue.inTransaction { db in
+                try db.create(table: "test") { t in
                     t.column("id", .Integer).primaryKey(ordering: .Desc, onConflict: .Fail)
                 }
-                XCTAssertEqual(self.lastSQLQuery, "CREATE TEMPORARY TABLE IF NOT EXISTS \"test\" (\"id\" INTEGER PRIMARY KEY DESC ON CONFLICT FAIL) WITHOUT ROWID")
+                XCTAssertEqual(self.lastSQLQuery,
+                    "CREATE TABLE \"test\" (" +
+                        "\"id\" INTEGER PRIMARY KEY DESC ON CONFLICT FAIL" +
+                    ")")
+                return .Rollback
+            }
+            try dbQueue.inTransaction { db in
+                try db.create(table: "test") { t in
+                    t.column("id", .Integer).primaryKey(autoincrement: true)
+                }
+                XCTAssertEqual(self.lastSQLQuery,
+                    "CREATE TABLE \"test\" (" +
+                        "\"id\" INTEGER PRIMARY KEY AUTOINCREMENT" +
+                    ")")
+                return .Rollback
+            }
+        }
+    }
+    
+    func testDropTable() {
+        assertNoError {
+            let dbQueue = try makeDatabaseQueue()
+            try dbQueue.inDatabase { db in
+                try db.create(table: "test") { t in
+                    t.column("id", .Integer).primaryKey()
+                    t.column("name", .Text)
+                }
+                XCTAssertTrue(db.tableExists("test"))
+                
+                try db.drop(table: "test")
+                XCTAssertEqual(self.lastSQLQuery, "DROP TABLE \"test\"")
+                XCTAssertFalse(db.tableExists("test"))
             }
         }
     }
