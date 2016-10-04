f9e68a0c9c690f15a340920a9bb1e4ffd0efca16
Gwendal Roué <gr@pierlis.com>
2016-10-04 11:43:15 +0200

    #92: FTS3 module documentation

--- GRDB/FTS/FTS3Module.swift
+++ GRDB/FTS/FTS3Module.swift
@@ -1,43 +1,52 @@
+/// FTS3 lets you define FTS3 virtual tables.
+///
+///     try db.create(virtualTable: "documents", using: FTS3()) { t in
+///         t.column("content")
+///     }
 public struct FTS3 : VirtualTableModule {
-    public let moduleName = "FTS3"
-    
+    /// Creates a FTS3 module suitable for the Database
+    /// `create(virtualTable:using:)` method.
+    ///
+    ///     try db.create(virtualTable: "documents", using: FTS3()) { t in
+    ///         t.column("content")
+    ///     }
     public init() {
     }
     
+    // MARK: - VirtualTableModule Adoption
+    
+    /// The virtual table module name
+    public let moduleName = "FTS3"
+    
+    /// Don't use this method.
     public func makeTableDefinition() -> FTS3TableDefinition {
         return FTS3TableDefinition()
     }
     
+    /// Don't use this method.
     public func moduleArguments(_ definition: FTS3TableDefinition) -> [String] {
         return definition.columns
     }
 }
 
-/// The TableDefinition class lets you define table columns and constraints.
+/// The FTS3TableDefinition class lets you define columns of a FTS3 virtual table.
 ///
 /// You don't create instances of this class. Instead, you use the Database
-/// `create(table:)` method:
+/// `create(virtualTable:using:)` method:
 ///
-///     try db.create(table: "persons") { t in // t is TableDefinition
-///         t.column(...)
+///     try db.create(virtualTable: "documents", using: FTS3()) { t in // t is FTS3TableDefinition
+///         t.column("content")
 ///     }
-///
-/// See https://www.sqlite.org/lang_createtable.html
 public final class FTS3TableDefinition : VirtualTableDefinition {
     fileprivate var columns: [String] = []
     
     /// Appends a table column.
     ///
-    ///     try db.create(virtualTable: "persons", using: TODO) { t in
-    ///         t.column("name")
+    ///     try db.create(virtualTable: "documents", using: FTS3()) { t in
+    ///         t.column("content")
     ///     }
     ///
-    /// See https://www.sqlite.org/lang_createtable.html#tablecoldef
-    ///
     /// - parameter name: the column name.
-    /// - parameter type: the column type.
-    /// - returns: An ColumnDefinition that allows you to refine the
-    ///   column definition.
     public func column(_ name: String) {
         columns.append(name)
     }
