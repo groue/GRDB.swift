a28b3cc2682ac9e6032b04827d045185efb38f88
Gwendal Roué <gr@pierlis.com>
2016-10-10 13:51:58 +0200

    FTS5.api robustness

--- GRDB/FTS/FTS5.swift
+++ GRDB/FTS/FTS5.swift
@@ -71,11 +71,19 @@
         }
         
         static func api(_ db: Database) -> UnsafePointer<fts5_api>? {
-            return Data
-                .fetchOne(db, "SELECT fts5()")
-                .flatMap { data in
-                    guard data.count == MemoryLayout<UnsafePointer<fts5_api>>.size else { return nil }
-                    return data.withUnsafeBytes { (api: UnsafePointer<UnsafePointer<fts5_api>>) in api.pointee }
+            do {
+                return try Data
+                    .fetch(db, "SELECT fts5()")
+                    .makeIterator()
+                    .step()
+                    .flatMap { data in
+                        guard data.count == MemoryLayout<UnsafePointer<fts5_api>>.size else {
+                            return nil
+                        }
+                        return data.withUnsafeBytes { $0.pointee }
+                }
+            } catch {
+                return nil
             }
         }
     }
