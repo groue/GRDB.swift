ba50771007e0fcc3d7c06006963478ca69fdfa38
David Hart <david@hartbit.com>
2017-02-22 23:02:44 +0100

    add tests

---
+++ Tests/Public/Record/RecordPersistenceConflictPolicy.swift
@@ -0,0 +1,35 @@
+import XCTest
+#if USING_SQLCIPHER
+    import GRDBCipher
+#elseif USING_CUSTOMSQLITE
+    import GRDBCustomSQLite
+#else
+    import GRDB
+#endif
+
+class RecordWithoutPersistenceConflictPolicy : Record {
+}
+
+class RecordWithPersistenceConflictPolicy : Record {
+    override class var persistenceConflictPolicy: PersistenceConflictPolicy {
+        return PersistenceConflictPolicy(insert: .fail, update: .ignore)
+    }
+}
+
+class RecordPersistenceConflictPolicyTests: GRDBTestCase {
+    
+    func testDefaultPersistenceConflictPolicy() {
+        let record = RecordWithoutPersistenceConflictPolicy()
+        let policy = type(of: record as MutablePersistable).persistenceConflictPolicy
+        XCTAssertEqual(policy.conflictResolutionForInsert, .abort)
+        XCTAssertEqual(policy.conflictResolutionForUpdate, .abort)
+    }
+    
+    func testConfigurablePersistenceConflictPolicy() {
+        let record = RecordWithPersistenceConflictPolicy()
+        let policy = type(of: record as MutablePersistable).persistenceConflictPolicy
+        XCTAssertEqual(policy.conflictResolutionForInsert, .fail)
+        XCTAssertEqual(policy.conflictResolutionForUpdate, .ignore)
+    }
+
+}
