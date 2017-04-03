e17c4e52714e2b1c36321750dbcbde6891e7f31e
Gwendal Roué <gr@pierlis.com>
2017-04-03 13:10:24 +0200

    GRDBDemoWatchOS

---
+++ DemoApps/GRDBDemoiOS/GRDBDemoWatchOS Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  GRDBDemoWatchOS Extension
+//
+//  Created by Gwendal Roué on 03/04/2017.
+//  Copyright © 2017 Gwendal Roué. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
