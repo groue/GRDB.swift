04654a5c8539a4d98d16ff6e47940ca3b4ed0ecd
Gwendal Roué <gr@pierlis.com>
2017-04-03 13:37:36 +0200

    Functional GRDBDemoWatchOS

--- DemoApps/GRDBDemoiOS/GRDBDemoWatchOS Extension/InterfaceController.swift
+++ DemoApps/GRDBDemoiOS/GRDBDemoWatchOS Extension/InterfaceController.swift
@@ -8,24 +8,16 @@
 
 import WatchKit
 import Foundation
-
+import GRDB
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet var versionLabel: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        let sqliteVersion = String(cString: sqlite3_libversion(), encoding: .utf8)
+        versionLabel.setText(sqliteVersion)
     }
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
 }
