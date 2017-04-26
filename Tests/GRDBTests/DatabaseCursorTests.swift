53ca2f485e0c74d452b6c1d637269047294e621b
Andrey Fidrya <af@zabiyaka.com>
2017-04-26 21:40:17 +0300

    Preserve domain and code when bridging NSError to DatabaseError (fixes failing testFunctionThrowingCustomError). Breaks testStepError on Linux -- also fixed.

--- Tests/GRDBTests/DatabaseCursorTests.swift
+++ Tests/GRDBTests/DatabaseCursorTests.swift
@@ -44,9 +44,15 @@ class DatabaseCursorTests: GRDBTestCase {
                 XCTFail()
             } catch let error as DatabaseError {
                 XCTAssertEqual(error.resultCode, .SQLITE_ERROR)
-                XCTAssertEqual(error.message, "\(customError)")
                 XCTAssertEqual(error.sql!, "SELECT throw()")
+                #if os(Linux)
+                // "\(customError)" doesn't contain domain and code on Linux, but DatabaseError's message should contain them
+                XCTAssertTrue(error.message!.contains("Custom"))
+                XCTAssertTrue(error.message!.contains(String(0xDEAD)))
+                #else
+                XCTAssertEqual(error.message, "\(customError)")
                 XCTAssertEqual(error.description, "SQLite error 1 with statement `SELECT throw()`: \(customError)")
+                #endif
             }
         }
     }
