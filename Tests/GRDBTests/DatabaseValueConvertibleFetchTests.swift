3be1fe5e505a9390885eedf9e0d359f0d52f9fbe
Andrey Fidrya <af@zabiyaka.com>
2017-04-26 22:31:44 +0300

    Partially fix testFetchCursorStepFail on Linux

--- Tests/GRDBTests/DatabaseValueConvertibleFetchTests.swift
+++ Tests/GRDBTests/DatabaseValueConvertibleFetchTests.swift
@@ -113,9 +113,18 @@ class DatabaseValueConvertibleFetchTests: GRDBTestCase {
                     XCTFail()
                 } catch let error as DatabaseError {
                     XCTAssertEqual(error.resultCode, .SQLITE_ERROR)
-                    XCTAssertEqual(error.message, "\(customError)")
                     XCTAssertEqual(error.sql!, sql)
+                    #if os(Linux)
+                    // "\(customError)" doesn't contain domain and code on Linux, but DatabaseError's message should contain them
+                    XCTAssertTrue(error.message!.contains("Custom"))
+                    XCTAssertTrue(error.message!.contains(String(0xDEAD)))
+                    XCTAssertTrue(error.description.hasPrefix("SQLite error 1 with statement `\(sql)`: "))
+                    XCTAssertTrue(error.description.contains("Custom"))
+                    XCTAssertTrue(error.description.contains(String(0xDEAD)))
+                    #else
+                    XCTAssertEqual(error.message, "\(customError)")
                     XCTAssertEqual(error.description, "SQLite error 1 with statement `\(sql)`: \(customError)")
+                    #endif
                 }
                 do {
                     _ = try cursor.next()
