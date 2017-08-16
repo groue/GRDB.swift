b77b3672ec8e21ebb7e31b1e982a751c63bf088a
Gwendal Roué <gr@pierlis.com>
2017-08-16 14:24:48 +0200

    Swift 4 + SPM
    - Upgrade Tests/SPM package layout and format to version 4, and have `make test_install_SPM` succeed
    - Update .gitignore in order to ignore Package.resolved instead of Package.pins

--- Tests/SPM/Package.swift
+++ Tests/SPM/Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SPM",
     dependencies: [
-        .Package(url: "https://github.com/groue/GRDB.swift.git", majorVersion: 0)
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "1.2.0"),
+    ],
+    targets: [
+        .target(name: "SPM", dependencies: ["GRDB"]),
     ]
 )
