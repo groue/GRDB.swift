9930fd3938aafb37362107cca1d50cc2f9437091
Gwendal Roué <gr@pierlis.com>
2017-10-11 08:17:57 +0200

    Fix failing test for drop temporary table

--- GRDB/Core/Database.swift
+++ GRDB/Core/Database.swift
@@ -1549,11 +1549,11 @@ final class StatementCompilationAuthorizer : StatementAuthorizer {
         // print("StatementCompilationAuthorizer: \(actionCode) \([cString1, cString2, cString3, cString4].flatMap { $0.map({ String(cString: $0) }) })")
         
         switch actionCode {
-        case SQLITE_DROP_TABLE, SQLITE_DROP_VTABLE:
+        case SQLITE_DROP_TABLE, SQLITE_DROP_VTABLE, SQLITE_DROP_TEMP_TABLE:
             isDropTableStatement = true
             invalidatesDatabaseSchemaCache = true
             return SQLITE_OK
-        case SQLITE_DROP_TEMP_TABLE, SQLITE_DROP_TEMP_VIEW, SQLITE_DROP_VIEW, SQLITE_DETACH, SQLITE_ALTER_TABLE, SQLITE_CREATE_INDEX, SQLITE_CREATE_TEMP_INDEX, SQLITE_DROP_INDEX, SQLITE_DROP_TEMP_INDEX:
+        case SQLITE_DROP_TEMP_VIEW, SQLITE_DROP_VIEW, SQLITE_DETACH, SQLITE_ALTER_TABLE, SQLITE_CREATE_INDEX, SQLITE_CREATE_TEMP_INDEX, SQLITE_DROP_INDEX, SQLITE_DROP_TEMP_INDEX:
             invalidatesDatabaseSchemaCache = true
             return SQLITE_OK
         case SQLITE_READ:
@@ -1575,6 +1575,7 @@ final class StatementCompilationAuthorizer : StatementAuthorizer {
             guard !isDropTableStatement else { return SQLITE_OK }
             guard let cString1 = cString1 else { return SQLITE_OK }
             guard strcmp(cString1, "sqlite_master") != 0 else { return SQLITE_OK }
+            guard strcmp(cString1, "sqlite_temp_master") != 0 else { return SQLITE_OK }
             databaseEventKinds.append(.delete(tableName: String(cString: cString1)))
             // Prevent the [truncate optimization](https://www.sqlite.org/lang_delete.html#truncateopt)
             // See also TruncateOptimizationBlocker
