6043b4288e231e0115e9eaf9471234095ba3b91b
Gwendal Roué <gr@pierlis.com>
2018-01-27 13:03:34 +0100

    SE-0184: backport UnsafeMutablePointer.deallocate()

--- GRDB/Legacy/Swift4.1.swift
+++ GRDB/Legacy/Swift4.1.swift
@@ -3,12 +3,21 @@
 #if swift(>=4.1)
 #else
 extension Sequence {
+    // From future import SE-0187
     func compactMap<ElementOfResult>(_ transform: (Self.Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
         return try flatMap(transform)
     }
 }
 
+extension UnsafeMutablePointer {
+    // From future import SE-0184
+    func deallocate() {
+        deallocate(capacity: 0)
+    }
+}
+
 extension UnsafeMutableRawBufferPointer {
+    // From future import SE-0184
     func copyMemory<C>(from source: C) where C : Collection, C.Element == UInt8 {
         copyBytes(from: source)
     }
