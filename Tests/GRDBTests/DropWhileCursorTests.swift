bae8c97df413a4ca1a2f90c0448ebc952ed0fd2b
Gwendal Roué <gr@pierlis.com>
2018-01-28 14:09:21 +0100

    Tests for Cursor.drop(while:)

---
+++ Tests/GRDBTests/DropWhileCursorTests.swift
@@ -0,0 +1,46 @@
+import XCTest
+#if GRDBCIPHER
+    import GRDBCipher
+#elseif GRDBCUSTOMSQLITE
+    import GRDBCustomSQLite
+#else
+    import GRDB
+#endif
+
+private struct TestError : Error { }
+
+class DropWhileCursorTests: GRDBTestCase {
+    
+    func testDropWhileCursorFromCursor() throws {
+        let base = IteratorCursor([1, 2, 3, 1, 5])
+        let cursor = base.drop(while: { $0 < 3 })
+        var x = try cursor.next()!
+        XCTAssertEqual(x, 3)
+        x = try cursor.next()!
+        XCTAssertEqual(x, 1)
+        x = try cursor.next()!
+        XCTAssertEqual(x, 5)
+        XCTAssertTrue(try cursor.next() == nil) // end
+        XCTAssertTrue(try cursor.next() == nil) // past the end
+    }
+    
+    func testDropWhileCursorFromThrowingCursor() throws {
+        var i = 0
+        let base: AnyCursor<Int> = AnyCursor {
+            guard i < 4 else { throw TestError() }
+            defer { i += 1 }
+            return i
+        }
+
+        let cursor = base.drop(while: { $0 < 3 })
+        var x = try cursor.next()!
+        XCTAssertEqual(x, 3)
+        do {
+            _ = try cursor.next()
+            XCTFail()
+        } catch is TestError {
+        } catch {
+            XCTFail()
+        }
+    }
+}
