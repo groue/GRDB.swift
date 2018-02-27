487f4e220f8a2933f15dd812fa1123d93d86ea1c
Gwendal Roué <gr@pierlis.com>
2018-02-27 20:10:13 +0100

    Record protocols fixit

--- GRDB/Legacy/Fixits-3.0.swift
+++ GRDB/Legacy/Fixits-3.0.swift
@@ -1,3 +1,14 @@
 @available(*, unavailable, renamed:"FetchRequest")
 public typealias Request = FetchRequest
 
+@available(*, unavailable, renamed:"FetchableRecord")
+public typealias RowConvertible = FetchableRecord
+
+@available(*, unavailable, renamed:"TableRecord")
+public typealias TableMapping = TableRecord
+
+@available(*, unavailable, renamed:"MutablePersistableRecord")
+public typealias MutablePersistable = MutablePersistableRecord
+
+@available(*, unavailable, renamed:"PersistableRecord")
+public typealias Persistable = PersistableRecord
