ad8b80bafd7d6e88b394a4f60db2a795821d0d96
Gwendal Roué <gr@pierlis.com>
2018-04-20 13:32:50 +0200

    (whitespace)

--- GRDB/Core/Support/Foundation/SQLiteDateParser.swift
+++ GRDB/Core/Support/Foundation/SQLiteDateParser.swift
@@ -13,10 +13,10 @@ class SQLiteDateParser {
         var second: Int32 = 0
         var nanosecond = ContiguousArray<CChar>.init(repeating: 0, count: 11)
     }
-
+    
     func components(from dateString: String) -> DatabaseDateComponents? {
         guard dateString.count >= 5 else { return nil }
-
+        
         if dateString[dateString.index(dateString.startIndex, offsetBy: 4)] == "-" {
             /***
              Note: A date string with full nanosecond precision is 29 chars.
@@ -24,7 +24,7 @@ class SQLiteDateParser {
              */
             return datetimeComponents(from: String(dateString.prefix(23)))
         }
-
+        
         if dateString[dateString.index(dateString.startIndex, offsetBy: 2)] == ":" {
             /***
              Note: A time string with full nanosecond precision is 18 chars.
@@ -32,10 +32,10 @@ class SQLiteDateParser {
              */
             return timeComponents(from: String(dateString.prefix(12)))
         }
-
+        
         return nil
     }
-
+    
     // - YYYY-MM-DD
     // - YYYY-MM-DD HH:MM
     // - YYYY-MM-DD HH:MM:SS
@@ -65,30 +65,30 @@ class SQLiteDateParser {
                 }
             }
         }
-
+        
         guard parseCount >= 3 else { return nil }
-
+        
         var components = DateComponents()
         components.year = Int(parserComponents.year)
         components.month = Int(parserComponents.month)
         components.day = Int(parserComponents.day)
-
+        
         guard parseCount >= 5 else { return DatabaseDateComponents(components, format: .YMD) }
-
+        
         components.hour = Int(parserComponents.hour)
         components.minute = Int(parserComponents.minute)
-
+        
         guard parseCount >= 6 else { return DatabaseDateComponents(components, format: .YMD_HM) }
-
+        
         components.second = Int(parserComponents.second)
-
+        
         guard parseCount >= 7 else { return DatabaseDateComponents(components, format: .YMD_HMS) }
-
+        
         components.nanosecond = nanosecondsInt(for: parserComponents.nanosecond)
-
+        
         return DatabaseDateComponents(components, format: .YMD_HMSS)
     }
-
+    
     // - HH:MM
     // - HH:MM:SS
     // - HH:MM:SS.SSS
@@ -109,22 +109,22 @@ class SQLiteDateParser {
         }
         
         guard parseCount >= 2 else { return nil }
-
+        
         var components = DateComponents()
         components.hour = Int(parserComponents.hour)
         components.minute = Int(parserComponents.minute)
-
+        
         guard parseCount >= 3 else { return DatabaseDateComponents(components, format: .HM) }
-
+        
         components.second = Int(parserComponents.second)
-
+        
         guard parseCount >= 4 else { return DatabaseDateComponents(components, format: .HMS) }
-
+        
         components.nanosecond = nanosecondsInt(for: parserComponents.nanosecond)
-
+        
         return DatabaseDateComponents(components, format: .HMSS)
     }
-
+    
     private func nanosecondsInt(for nanosecond: ContiguousArray<CChar>) -> Int {
         let nanoString = "0." + nanosecond.withUnsafeBufferPointer { String(cString: $0.baseAddress!) }
         guard let doubleValue = Double(nanoString) else { return 0 }
