27e94c211d1e6b129ecfa9e73d5846717ceed4e8
Gwendal Roué <gr@pierlis.com>
2018-06-07 13:53:16 +0200

    Upgrade SPM test app to v3.0.0

--- Tests/SPM/Package.swift
+++ Tests/SPM/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SPM",
     dependencies: [
-        .package(url: "https://github.com/groue/GRDB.swift.git", from: "2.0.1"),
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "SPM", dependencies: ["GRDB"]),
