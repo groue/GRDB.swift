1c66ad4aaddb42e34ec5f35291f955a3786a65c3
Gwendal Roué <gr@pierlis.com>
2018-08-15 12:05:16 +0200

    Restore GRDB/Core/Support/Foundation/Data.swift
    because Data belongs to Foundation, not to the Standard Library.
    
    Remove `import Foundation` from StandardLibrary.swift

---
+++ GRDB/Core/Support/Foundation/Data.swift
@@ -0,0 +1,38 @@
+import Foundation
+#if SWIFT_PACKAGE
+    import CSQLite
+#elseif !GRDBCUSTOMSQLITE && !GRDBCIPHER
+    import SQLite3
+#endif
+
+/// Data is convertible to and from DatabaseValue.
+extension Data : DatabaseValueConvertible, StatementColumnConvertible {
+    public init(sqliteStatement: SQLiteStatement, index: Int32) {
+        if let bytes = sqlite3_column_blob(sqliteStatement, Int32(index)) {
+            let count = Int(sqlite3_column_bytes(sqliteStatement, Int32(index)))
+            self.init(bytes: bytes, count: count) // copy bytes
+        } else {
+            self.init()
+        }
+    }
+    
+    /// Returns a value that can be stored in the database.
+    public var databaseValue: DatabaseValue {
+        return DatabaseValue(storage: .blob(self))
+    }
+    
+    /// Returns a Data initialized from *dbValue*, if it contains
+    /// a Blob.
+    public static func fromDatabaseValue(_ dbValue: DatabaseValue) -> Data? {
+        switch dbValue.storage {
+        case .blob(let data):
+            return data
+        case .string(let string):
+            // Implicit conversion from string to blob, just as SQLite does
+            // See https://www.sqlite.org/c3ref/column_blob.html
+            return string.data(using: .utf8)
+        default:
+            return nil
+        }
+    }
+}
