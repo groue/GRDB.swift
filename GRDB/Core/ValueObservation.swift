bb02d9d857f8eb1bd9c00a5991613dfbd8aad318
Gwendal Roué <gr@pierlis.com>
2018-10-31 12:07:08 +0100

    ValueObservation.map

--- GRDB/Core/ValueObservation.swift
+++ GRDB/Core/ValueObservation.swift
@@ -93,6 +93,37 @@ public protocol ValueReducer {
     mutating func value(_ fetched: Fetched) -> Value?
 }
 
+extension ValueReducer {
+    public func map<T>(_ transform: @escaping (Value) -> T?) -> MapValueReducer<Self, T> {
+        return MapValueReducer(self, transform)
+    }
+}
+
+/// A ValueReducer whose values consist of those in a Base ValueReducer passed
+/// through a transform function returning T.
+///
+/// See ValueReducer.map(_:)
+///
+/// :nodoc:
+public struct MapValueReducer<Base: ValueReducer, T>: ValueReducer {
+    private var base: Base
+    private let transform: (Base.Value) -> T?
+
+    init(_ base: Base, _ transform: @escaping (Base.Value) -> T?) {
+        self.base = base
+        self.transform = transform
+    }
+
+    public func fetch(_ db: Database) throws -> Base.Fetched {
+        return try base.fetch(db)
+    }
+
+    public mutating func value(_ fetched: Base.Fetched) -> T? {
+        guard let value = base.value(fetched) else { return nil }
+        return transform(value)
+    }
+}
+
 /// TODO: doc
 public struct AnyValueReducer<Fetched, Value>: ValueReducer {
     var _fetch: (Database) throws -> Fetched
@@ -430,6 +461,13 @@ public struct ValueObservation<Reducer> {
         -> ValueObservation
     {
         return ValueObservation(tracking: union(regions), reducer: reducer)
+    }
+}
+
+extension ValueObservation where Reducer: ValueReducer {
+    /// TODO
+    public func map<T>(_ transform: @escaping (Reducer.Value) -> T) -> ValueObservation<MapValueReducer<Reducer, T>> {
+        return ValueObservation<MapValueReducer<Reducer, T>>(tracking: observedRegion, reducer: reducer.map(transform))
     }
 }
 
