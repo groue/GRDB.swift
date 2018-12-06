862563fdaad93053d5ba1c4eece6056461e6a0a0
Gwendal Roué <gr@pierlis.com>
2018-12-06 16:42:20 +0100

    Remove obsolete test

--- Tests/GRDBTests/DatabaseRegionObservationTests.swift
+++ Tests/GRDBTests/DatabaseRegionObservationTests.swift
@@ -13,47 +13,6 @@ import XCTest
 #endif
 
 class DatabaseRegionObservationTests: GRDBTestCase {
-    func testDatabaseRegionObservation() throws {
-        let dbQueue = try makeDatabaseQueue()
-        try dbQueue.write { try $0.execute("CREATE TABLE t(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)") }
-        
-        var results: [[Row]] = []
-        let notificationExpectation = expectation(description: "notification")
-        notificationExpectation.assertForOverFulfill = true
-        notificationExpectation.expectedFulfillmentCount = 4
-        
-        let request = SQLRequest<Row>("SELECT * FROM t ORDER BY id")
-        let observation = DatabaseRegionObservation(tracking: { db in
-            try request.databaseRegion(db)
-        })
-        let observer = try observation.start(in: dbQueue) { db in
-            let rows = try! request.fetchAll(db)
-            results.append(rows)
-            notificationExpectation.fulfill()
-        }
-        try withExtendedLifetime(observer) {
-            try dbQueue.inDatabase { db in
-                try db.execute("INSERT INTO t (id, name) VALUES (1, 'foo')") // +1
-                try db.execute("UPDATE t SET name = 'foo' WHERE id = 1")     // =
-                try db.inTransaction {                                       // +1
-                    try db.execute("INSERT INTO t (id, name) VALUES (2, 'bar')")
-                    try db.execute("INSERT INTO t (id, name) VALUES (3, 'baz')")
-                    try db.execute("DELETE FROM t WHERE id = 3")
-                    return .commit
-                }
-                try db.execute("DELETE FROM t WHERE id = 1")                 // -1
-            }
-            
-            waitForExpectations(timeout: 1, handler: nil)
-        }
-        
-        XCTAssertEqual(results, [
-            [["id":1, "name":"foo"]],
-            [["id":1, "name":"foo"]],
-            [["id":1, "name":"foo"], ["id":2, "name":"bar"]],
-            [["id":2, "name":"bar"]]])
-    }
-    
     func testDatabaseRegionObservationVariadic() throws {
         let dbQueue = try makeDatabaseQueue()
         try dbQueue.write {
