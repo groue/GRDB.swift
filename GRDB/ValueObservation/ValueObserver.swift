197f922e7e3ca9ce47b395bf7a6b73308fb167c6
Gwendal Roué <gr@pierlis.com>
2018-12-08 17:11:53 +0100

    Fix failing tests for over-retained ValueObserver

--- GRDB/ValueObservation/ValueObserver.swift
+++ GRDB/ValueObservation/ValueObserver.swift
@@ -52,32 +52,28 @@ class ValueObserver<Reducer: ValueReducer>: TransactionObserver {
         // - that notifications have the same ordering as transactions.
         // - that expensive reduce operations are computed without blocking
         // any database dispatch queue.
-        reduceQueue.async { [weak self] in
-            guard let strongSelf = self else { return }
-            
+        reduceQueue.async { [weak self] /* never ever retain self */ in
             do {
-                if let value = try strongSelf.reducer.value(future.wait()) {
-                    if let queue = strongSelf.notificationQueue {
+                if let value = try self?.reducer.value(future.wait()) {
+                    if let queue = self?.notificationQueue {
                         queue.async {
-                            guard let strongSelf = self else { return }
-                            strongSelf.onChange(value)
+                            self?.onChange(value)
                         }
                     } else {
-                        strongSelf.onChange(value)
+                        self?.onChange(value)
                     }
                 }
             } catch {
-                guard strongSelf.onError != nil else {
+                guard self?.onError != nil else {
                     // TODO: how can we let the user know about the error?
                     return
                 }
-                if let queue = strongSelf.notificationQueue {
+                if let queue = self?.notificationQueue {
                     queue.async {
-                        guard let strongSelf = self else { return }
-                        strongSelf.onError?(error)
+                        self?.onError?(error)
                     }
                 } else {
-                    strongSelf.onError?(error)
+                    self?.onError?(error)
                 }
             }
         }
@@ -85,5 +81,4 @@ class ValueObserver<Reducer: ValueReducer>: TransactionObserver {
     
     func databaseDidRollback(_ db: Database) {
         isChanged = false
-    }
-}
+    }}
