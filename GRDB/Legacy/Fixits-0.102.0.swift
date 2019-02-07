ad04ccc09457ac91134d27d4e164ae9905d09804
Gwendal Roué <gr@pierlis.com>
2019-02-07 12:52:52 +0100

    Drop all fixits for GRDB < 3.0

--- GRDB/Legacy/Fixits-0.102.0.swift
+++
@@ -1,9 +0,0 @@
-extension Database {
-    @available(*, unavailable, renamed:"inTransaction")
-    public func writeInTransaction(_ kind: Database.TransactionKind? = nil, _ block: (Database) throws -> Database.TransactionCompletion) throws { }
-}
-
-extension DatabaseValue {
-    @available(*, unavailable, message:"DatabaseSequence has been replaced by Cursor.")
-    public func value() -> Any { preconditionFailure() }
-}
