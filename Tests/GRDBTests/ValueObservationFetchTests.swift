bb045235bfd346c22f050c7ddb30f640870f240c
Gwendal Roué <gr@pierlis.com>
2019-02-08 10:15:49 +0100

    Remove tests for deprecated methods

--- Tests/GRDBTests/ValueObservationFetchTests.swift
+++ Tests/GRDBTests/ValueObservationFetchTests.swift
@@ -16,10 +16,8 @@ class ValueObservationFetchTests: GRDBTestCase {
     func testRegionsAPI() {
         // single region
         _ = ValueObservation.tracking(DatabaseRegion(), fetch: { _ in })
-        _ = ValueObservation.tracking(DatabaseRegion(), fetchDistinct: { _ in 0 })
         // variadic
         _ = ValueObservation.tracking(DatabaseRegion(), DatabaseRegion(), fetch: { _ in })
-        _ = ValueObservation.tracking(DatabaseRegion(), DatabaseRegion(), fetchDistinct: { _ in 0 })
         // array
         _ = ValueObservation.tracking([DatabaseRegion()], fetch: { _ in })
     }
@@ -56,39 +54,6 @@ class ValueObservationFetchTests: GRDBTestCase {
         try test(makeDatabasePool())
     }
     
-    // Test for a deprecated API
-    func testFetchDistinct() throws {
-        func test(_ dbWriter: DatabaseWriter) throws {
-            try dbWriter.write { try $0.execute("CREATE TABLE t(id INTEGER PRIMARY KEY AUTOINCREMENT)") }
-            
-            var counts: [Int] = []
-            let notificationExpectation = expectation(description: "notification")
-            notificationExpectation.assertForOverFulfill = true
-            notificationExpectation.expectedFulfillmentCount = 3
-            
-            var observation = ValueObservation.tracking(DatabaseRegion.fullDatabase, fetchDistinct: {
-                try Int.fetchOne($0, "SELECT COUNT(*) FROM t")!
-            })
-            observation.extent = .databaseLifetime
-            _ = try observation.start(in: dbWriter) { count in
-                counts.append(count)
-                notificationExpectation.fulfill()
-            }
-            
-            try dbWriter.writeWithoutTransaction { db in
-                try db.execute("INSERT INTO t DEFAULT VALUES")
-                try db.execute("UPDATE t SET id = id")
-                try db.execute("INSERT INTO t DEFAULT VALUES")
-            }
-            
-            waitForExpectations(timeout: 1, handler: nil)
-            XCTAssertEqual(counts, [0, 1, 2])
-        }
-        
-        try test(makeDatabaseQueue())
-        try test(makeDatabasePool())
-    }
-
     func testDistinctUntilChanged() throws {
         func test(_ dbWriter: DatabaseWriter) throws {
             try dbWriter.write { try $0.execute("CREATE TABLE t(id INTEGER PRIMARY KEY AUTOINCREMENT)") }
