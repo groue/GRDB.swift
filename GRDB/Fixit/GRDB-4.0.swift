efd69a628a95a61c2d3f16a96e964914d577f3f8
Gwendal Roué <gr@pierlis.com>
2019-02-17 12:50:29 +0100

    Add fixit for Cursor.flatMap

--- GRDB/Fixit/GRDB-4.0.swift
+++ GRDB/Fixit/GRDB-4.0.swift
@@ -1,1 +1,6 @@
 // Fixits for changes introduced by GRDB 4.0.0
+
+extension Cursor {
+    @available(*, unavailable, renamed: "compactMap")
+    public func flatMap<ElementOfResult>(_ transform: @escaping (Element) throws -> ElementOfResult?) -> MapCursor<FilterCursor<MapCursor<Self, ElementOfResult?>>, ElementOfResult> { preconditionFailure() }
+}
