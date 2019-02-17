06ec4580be980d79f909e07f6695ff5aed01c21d
Gwendal Roué <gr@pierlis.com>
2019-02-17 12:59:32 +0100

    Fixit for DatabaseWriter.readFromCurrentState

--- GRDB/Fixit/GRDB-4.0.swift
+++ GRDB/Fixit/GRDB-4.0.swift
@@ -4,3 +4,8 @@ extension Cursor {
     @available(*, unavailable, renamed: "compactMap")
     public func flatMap<ElementOfResult>(_ transform: @escaping (Element) throws -> ElementOfResult?) -> MapCursor<FilterCursor<MapCursor<Self, ElementOfResult?>>, ElementOfResult> { preconditionFailure() }
 }
+
+extension DatabaseWriter {
+    @available(*, unavailable, message: "Use concurrentRead instead")
+    public func readFromCurrentState(_ block: @escaping (Database) -> Void) throws { preconditionFailure() }
+}
