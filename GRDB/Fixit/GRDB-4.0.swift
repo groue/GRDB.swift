e100287bd8ac6660673901b0722175bcfd572061
Gwendal Roué <gr@pierlis.com>
2019-02-17 13:07:50 +0100

    Fixits for ValueObservation

--- GRDB/Fixit/GRDB-4.0.swift
+++ GRDB/Fixit/GRDB-4.0.swift
@@ -10,6 +10,14 @@ extension DatabaseWriter {
     public func readFromCurrentState(_ block: @escaping (Database) -> Void) throws { preconditionFailure() }
 }
 
+extension ValueObservation {
+    @available(*, unavailable, message: "Provide the reducer in a (Database) -> Reducer closure")
+    public static func tracking(_ regions: DatabaseRegionConvertible..., reducer: Reducer) -> ValueObservation { preconditionFailure() }
+    
+    @available(*, unavailable, message: "Use distinctUntilChanged() instead")
+    public static func tracking<Value>(_ regions: DatabaseRegionConvertible..., fetchDistinct fetch: @escaping (Database) throws -> Value) -> ValueObservation<DistinctUntilChangedValueReducer<RawValueReducer<Value>>> where Value: Equatable { preconditionFailure() }
+}
+
 @available(*, unavailable, renamed: "FastDatabaseValueCursor")
 public typealias ColumnCursor<Value: DatabaseValueConvertible & StatementColumnConvertible> = FastDatabaseValueCursor<Value>
 
