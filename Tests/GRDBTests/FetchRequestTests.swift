65fe2bb50c2e87a73c5fa30577c2c51d74b6fcd2
Gwendal Roué <gr@pierlis.com>
2019-04-17 21:31:30 +0200

    Tests for the singleResult hint and fetching methods

--- Tests/GRDBTests/FetchRequestTests.swift
+++ Tests/GRDBTests/FetchRequestTests.swift
@@ -136,4 +136,208 @@ class FetchRequestTests: GRDBTestCase {
             XCTAssertEqual(count, 2)
         }
     }
+    
+    // Test for the `singleResult` parameter of
+    // FetchRequest.prepare(_:singleResult:)
+    func testSingleResultHint() throws {
+        struct CustomRequest<T>: FetchRequest {
+            typealias RowDecoder = T
+            func prepare(_ db: Database, forSingleResult singleResult: Bool) throws -> (SelectStatement, RowAdapter?) {
+                if singleResult {
+                    return try (db.makeSelectStatement(sql: "SELECT 'single' AS hint"), nil)
+                } else {
+                    return try (db.makeSelectStatement(sql: "SELECT 'multiple' AS hint"), nil)
+                }
+            }
+        }
+
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.read { db in
+            // Row
+            do {
+                do {
+                    let request = CustomRequest<Row>()
+                    do {
+                        let rows = try request.fetchAll(db)
+                        XCTAssertEqual(rows.count, 1)
+                        XCTAssertEqual(rows[0], ["hint": "multiple"])
+                    }
+                    do {
+                        let rows = try request.fetchCursor(db)
+                        while let row = try rows.next() {
+                            XCTAssertEqual(row, ["hint": "multiple"])
+                        }
+                    }
+                    do {
+                        let row = try request.fetchOne(db)!
+                        XCTAssertEqual(row, ["hint": "single"])
+                    }
+                }
+                do {
+                    let request = CustomRequest<Void>()
+                    do {
+                        let rows = try Row.fetchAll(db, request)
+                        XCTAssertEqual(rows.count, 1)
+                        XCTAssertEqual(rows[0], ["hint": "multiple"])
+                    }
+                    do {
+                        let rows = try Row.fetchCursor(db, request)
+                        while let row = try rows.next() {
+                            XCTAssertEqual(row, ["hint": "multiple"])
+                        }
+                    }
+                    do {
+                        let row = try Row.fetchOne(db, request)!
+                        XCTAssertEqual(row, ["hint": "single"])
+                    }
+                }
+            }
+            
+            // DatabaseValueConvertible
+            do {
+                struct Value: DatabaseValueConvertible {
+                    var string: String
+                    var databaseValue: DatabaseValue { return string.databaseValue }
+                    static func fromDatabaseValue(_ dbValue: DatabaseValue) -> Value? {
+                        return String.fromDatabaseValue(dbValue).map(Value.init)
+                    }
+                }
+                do {
+                    let request = CustomRequest<Value>()
+                    do {
+                        let values = try request.fetchAll(db)
+                        XCTAssertEqual(values.count, 1)
+                        XCTAssertEqual(values[0].string, "multiple")
+                    }
+                    do {
+                        let values = try request.fetchCursor(db)
+                        while let value = try values.next() {
+                            XCTAssertEqual(value.string, "multiple")
+                        }
+                    }
+                    do {
+                        let value = try request.fetchOne(db)!
+                        XCTAssertEqual(value.string, "single")
+                    }
+                }
+                do {
+                    let request = CustomRequest<Void>()
+                    do {
+                        let values = try Value.fetchAll(db, request)
+                        XCTAssertEqual(values.count, 1)
+                        XCTAssertEqual(values[0].string, "multiple")
+                    }
+                    do {
+                        let values = try Value.fetchCursor(db, request)
+                        while let value = try values.next() {
+                            XCTAssertEqual(value.string, "multiple")
+                        }
+                    }
+                    do {
+                        let value = try Value.fetchOne(db, request)!
+                        XCTAssertEqual(value.string, "single")
+                    }
+                }
+            }
+            
+            // DatabaseValueConvertible + StatementColumnConvertible
+            do {
+                struct Value: DatabaseValueConvertible, StatementColumnConvertible {
+                    var string: String
+                    init(string: String) {
+                        self.string = string
+                    }
+                    init(sqliteStatement: SQLiteStatement, index: Int32) {
+                        self.init(string: String(sqliteStatement: sqliteStatement, index: index))
+                    }
+                    var databaseValue: DatabaseValue { return string.databaseValue }
+                    static func fromDatabaseValue(_ dbValue: DatabaseValue) -> Value? {
+                        return String.fromDatabaseValue(dbValue).map { Value(string: $0) }
+                    }
+                }
+                do {
+                    let request = CustomRequest<Value>()
+                    do {
+                        let values = try request.fetchAll(db)
+                        XCTAssertEqual(values.count, 1)
+                        XCTAssertEqual(values[0].string, "multiple")
+                    }
+                    do {
+                        let values = try request.fetchCursor(db)
+                        while let value = try values.next() {
+                            XCTAssertEqual(value.string, "multiple")
+                        }
+                    }
+                    do {
+                        let value = try request.fetchOne(db)!
+                        XCTAssertEqual(value.string, "single")
+                    }
+                }
+                do {
+                    let request = CustomRequest<Void>()
+                    do {
+                        let values = try Value.fetchAll(db, request)
+                        XCTAssertEqual(values.count, 1)
+                        XCTAssertEqual(values[0].string, "multiple")
+                    }
+                    do {
+                        let values = try Value.fetchCursor(db, request)
+                        while let value = try values.next() {
+                            XCTAssertEqual(value.string, "multiple")
+                        }
+                    }
+                    do {
+                        let value = try Value.fetchOne(db, request)!
+                        XCTAssertEqual(value.string, "single")
+                    }
+                }
+            }
+            
+            // FetchableRecord
+            do {
+                struct Record: FetchableRecord {
+                    var string: String
+                    init(row: Row) {
+                        string = row[0]
+                    }
+                }
+                do {
+                    let request = CustomRequest<Record>()
+                    do {
+                        let records = try request.fetchAll(db)
+                        XCTAssertEqual(records.count, 1)
+                        XCTAssertEqual(records[0].string, "multiple")
+                    }
+                    do {
+                        let records = try request.fetchCursor(db)
+                        while let record = try records.next() {
+                            XCTAssertEqual(record.string, "multiple")
+                        }
+                    }
+                    do {
+                        let record = try request.fetchOne(db)!
+                        XCTAssertEqual(record.string, "single")
+                    }
+                }
+                do {
+                    let request = CustomRequest<Void>()
+                    do {
+                        let records = try Record.fetchAll(db, request)
+                        XCTAssertEqual(records.count, 1)
+                        XCTAssertEqual(records[0].string, "multiple")
+                    }
+                    do {
+                        let records = try Record.fetchCursor(db, request)
+                        while let record = try records.next() {
+                            XCTAssertEqual(record.string, "multiple")
+                        }
+                    }
+                    do {
+                        let record = try Record.fetchOne(db, request)!
+                        XCTAssertEqual(record.string, "single")
+                    }
+                }
+            }
+        }
+    }
 }
