f50a7656213b76ba48e45cdd7308a5cdbac64a2e
Gwendal Roué <gr@pierlis.com>
2019-05-30 16:42:26 +0200

    Tests for ValueObservation.fetch

--- Tests/GRDBTests/ValueObservationFetchTests.swift
+++ Tests/GRDBTests/ValueObservationFetchTests.swift
@@ -11,76 +11,133 @@ import XCTest
 #endif
 
 class ValueObservationFetchTests: GRDBTestCase {
-    func testRegionsAPI() {
-        // single region
-        _ = ValueObservation.tracking(DatabaseRegion(), fetch: { _ in })
-        // variadic
-        _ = ValueObservation.tracking(DatabaseRegion(), DatabaseRegion(), fetch: { _ in })
-        // array
-        _ = ValueObservation.tracking([DatabaseRegion()], fetch: { _ in })
+    override func setup(_ dbWriter: DatabaseWriter) throws {
+        try dbWriter.write { db in
+            try db.create(table: "player") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("name", .text)
+            }
+            try db.execute(sql: """
+                INSERT INTO player (name) VALUES ('Arthur');
+                INSERT INTO player (name) VALUES ('Barbara');
+                INSERT INTO player (name) VALUES (NULL);
+                """)
+        }
     }
     
-    func testFetch() throws {
-        func test(_ dbWriter: DatabaseWriter) throws {
-            try dbWriter.write { try $0.execute(sql: "CREATE TABLE t(id INTEGER PRIMARY KEY AUTOINCREMENT)") }
-            
-            var counts: [Int] = []
-            let notificationExpectation = expectation(description: "notification")
-            notificationExpectation.assertForOverFulfill = true
-            notificationExpectation.expectedFulfillmentCount = 4
-            
-            let observation = ValueObservation.tracking(DatabaseRegion.fullDatabase, fetch: {
-                try Int.fetchOne($0, sql: "SELECT COUNT(*) FROM t")!
-            })
-            let observer = try observation.start(in: dbWriter) { count in
-                counts.append(count)
-                notificationExpectation.fulfill()
-            }
-            try withExtendedLifetime(observer) {
-                try dbWriter.writeWithoutTransaction { db in
-                    try db.execute(sql: "INSERT INTO t DEFAULT VALUES")
-                    try db.execute(sql: "UPDATE t SET id = id")
-                    try db.execute(sql: "INSERT INTO t DEFAULT VALUES")
-                }
-                
-                waitForExpectations(timeout: 1, handler: nil)
-                XCTAssertEqual(counts, [0, 1, 1, 2])
-            }
+    func testFetchCount() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.read { db in
+            let request = SQLRequest<Row>(sql: "SELECT * FROM player")
+            let observation = ValueObservation.trackingCount(request)
+            let count: Int = try observation.fetch(db)
+            XCTAssertEqual(count, 3)
         }
-        
-        try test(makeDatabaseQueue())
-        try test(makeDatabasePool())
     }
     
-    func testDistinctUntilChanged() throws {
-        func test(_ dbWriter: DatabaseWriter) throws {
-            try dbWriter.write { try $0.execute(sql: "CREATE TABLE t(id INTEGER PRIMARY KEY AUTOINCREMENT)") }
-            
-            var counts: [Int] = []
-            let notificationExpectation = expectation(description: "notification")
-            notificationExpectation.assertForOverFulfill = true
-            notificationExpectation.expectedFulfillmentCount = 3
-            
-            let observation = ValueObservation.tracking(DatabaseRegion.fullDatabase, fetch: {
-                try Int.fetchOne($0, sql: "SELECT COUNT(*) FROM t")!
-            }).distinctUntilChanged()
-            let observer = try observation.start(in: dbWriter) { count in
-                counts.append(count)
-                notificationExpectation.fulfill()
+    func testFetchRow() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.read { db in
+            do {
+                let request = SQLRequest<Row>(sql: "SELECT * FROM player WHERE NULL")
+                let observation = ValueObservation.trackingOne(request)
+                let row: Row? = try observation.fetch(db)
+                XCTAssertNil(row)
             }
-            try withExtendedLifetime(observer) {
-                try dbWriter.writeWithoutTransaction { db in
-                    try db.execute(sql: "INSERT INTO t DEFAULT VALUES")
-                    try db.execute(sql: "UPDATE t SET id = id")
-                    try db.execute(sql: "INSERT INTO t DEFAULT VALUES")
-                }
-                
-                waitForExpectations(timeout: 1, handler: nil)
-                XCTAssertEqual(counts, [0, 1, 2])
+            do {
+                let request = SQLRequest<Row>(sql: "SELECT * FROM player ORDER BY id")
+                let observation = ValueObservation.trackingOne(request)
+                let row: Row? = try observation.fetch(db)
+                XCTAssertEqual(row, ["id": 1, "name": "Arthur"])
+            }
+            do {
+                let request = SQLRequest<Row>(sql: "SELECT * FROM player ORDER BY id")
+                let observation = ValueObservation.trackingAll(request)
+                let rows: [Row] = try observation.fetch(db)
+                XCTAssertEqual(rows, [
+                    ["id": 1, "name": "Arthur"],
+                    ["id": 2, "name": "Barbara"],
+                    ["id": 3, "name": nil],
+                    ])
             }
         }
-        
-        try test(makeDatabaseQueue())
-        try test(makeDatabasePool())
+    }
+    
+    func testFetchDatabaseValueConvertible() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.read { db in
+            do {
+                let request = SQLRequest<String>(sql: "SELECT name FROM player WHERE NULL")
+                let observation = ValueObservation.trackingOne(request)
+                let name: String? = try observation.fetch(db)
+                XCTAssertNil(name)
+            }
+            do {
+                let request = SQLRequest<String>(sql: "SELECT name FROM player WHERE name IS NULL")
+                let observation = ValueObservation.trackingOne(request)
+                let name: String? = try observation.fetch(db)
+                XCTAssertNil(name)
+            }
+            do {
+                let request = SQLRequest<String>(sql: "SELECT name FROM player WHERE name IS NOT NULL ORDER BY id")
+                let observation = ValueObservation.trackingOne(request)
+                let name: String? = try observation.fetch(db)
+                XCTAssertEqual(name, "Arthur")
+            }
+            do {
+                let request = SQLRequest<String>(sql: "SELECT name FROM player WHERE name IS NOT NULL ORDER BY id")
+                let observation = ValueObservation.trackingAll(request)
+                let names: [String] = try observation.fetch(db)
+                XCTAssertEqual(names, ["Arthur", "Barbara"])
+            }
+            do {
+                let request = SQLRequest<String?>(sql: "SELECT name FROM player ORDER BY id")
+                let observation = ValueObservation.trackingAll(request)
+                let names: [String?] = try observation.fetch(db)
+                XCTAssertEqual(names, ["Arthur", "Barbara", nil])
+            }
+        }
+    }
+    
+    func testFetchFetchableRecord() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.read { db in
+            struct Player: FetchableRecord, TableRecord, Equatable {
+                var id: Int64
+                var name: String?
+                
+                init(id: Int64, name: String?) {
+                    self.id = id
+                    self.name = name
+                }
+                
+                init(row: Row) {
+                    id = row["id"]
+                    name = row["name"]
+                }
+            }
+            do {
+                let request = Player.none()
+                let observation = ValueObservation.trackingOne(request)
+                let player: Player? = try observation.fetch(db)
+                XCTAssertNil(player)
+            }
+            do {
+                let request = Player.orderByPrimaryKey()
+                let observation = ValueObservation.trackingOne(request)
+                let player: Player? = try observation.fetch(db)
+                XCTAssertEqual(player, Player(id: 1, name: "Arthur"))
+            }
+            do {
+                let request = Player.orderByPrimaryKey()
+                let observation = ValueObservation.trackingAll(request)
+                let players: [Player] = try observation.fetch(db)
+                XCTAssertEqual(players, [
+                    Player(id: 1, name: "Arthur"),
+                    Player(id: 2, name: "Barbara"),
+                    Player(id: 3, name: nil),
+                    ])
+            }
+        }
     }
 }
