bb959fa5c32ba78ee185e8551085c96c5ba4b092
Gwendal Roué <gr@pierlis.com>
2019-08-02 14:14:48 +0200

    Add failing test for #583

--- Tests/GRDBTests/DatabaseCursorTests.swift
+++ Tests/GRDBTests/DatabaseCursorTests.swift
@@ -70,4 +70,62 @@ class DatabaseCursorTests: GRDBTestCase {
             }
         }
     }
+    
+    func testIssue583() throws {
+        struct User: Codable, TableRecord, FetchableRecord, MutablePersistableRecord {
+            static let databaseTableName: String = "user"
+            
+            var id: Int64?
+            var username: String
+            var isFlagged: Bool
+            
+            init(id: Int64? = nil, username: String, isFlagged: Bool = false) {
+                self.id = id
+                self.username = username
+                self.isFlagged = isFlagged
+            }
+            
+            mutating func didInsert(with rowID: Int64, for column: String?) {
+                id = rowID
+            }
+        }
+        
+        struct FlagUser: Codable, TableRecord, FetchableRecord, MutablePersistableRecord {
+            static let databaseTableName: String = "flagUser"
+            
+            var username: String
+        }
+        
+        let queue = try makeDatabaseQueue()
+        try queue.write { database in
+            try database.create(table: User.databaseTableName) { definition in
+                definition.column("id", .integer).primaryKey(autoincrement: true)
+                definition.column("username", .text).notNull()
+                definition.column("isFlagged", .boolean).notNull().defaults(to: false)
+            }
+            
+            try database.create(table: FlagUser.databaseTableName) { definition in
+                definition.column("username", .text).notNull()
+            }
+            
+            try [Int](0...50).forEach {
+                var user = User(username: "User\($0)")
+                try user.insert(database)
+            }
+            
+            try [Int](40...60).forEach {
+                var flag = FlagUser(username: "User\($0)")
+                try flag.insert(database)
+            }
+        }
+        
+        let query = "SELECT * FROM flagUser WHERE (SELECT COUNT(id) FROM user WHERE username = flagUser.username AND isFlagged = 1) = 0"
+        try queue.inDatabase { database in
+            let cursor = try FlagUser.fetchCursor(database, sql: query)
+            while let flagged = try cursor.next() {
+                _ = try User.fetchOne(database, sql: "SELECT * FROM user WHERE username = '\(flagged.username)' LIMIT 1") ??
+                    User(username: flagged.username)
+            }
+        }
+    }
 }
