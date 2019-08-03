731a83908d7ddcaf2603253db3b2018c4a461b93
Gwendal Roué <gr@pierlis.com>
2019-08-03 18:03:05 +0200

    Test automatic ValueObservation region tracking with various schedulings

--- Tests/GRDBTests/ValueObservationRegionRecordingTests.swift
+++ Tests/GRDBTests/ValueObservationRegionRecordingTests.swift
@@ -1,12 +1,12 @@
 import XCTest
 #if GRDBCUSTOMSQLITE
-    import GRDBCustomSQLite
+    @testable import GRDBCustomSQLite
 #else
-    import GRDB
+    @testable import GRDB
 #endif
 
 class ValueObservationRegionRecordingTests: GRDBTestCase {
-    func testRegionRecording() throws {
+    func testMainQueueScheduling() throws {
         let dbQueue = try makeDatabaseQueue()
         try dbQueue.write {
             try $0.execute(sql: """
@@ -32,6 +32,9 @@ class ValueObservationRegionRecordingTests: GRDBTestCase {
             notificationExpectation.fulfill()
         }
         
+        let token = observer as! ValueObserverToken<ValueReducers.Fetch<Int>> // Non-public implementation detail
+        XCTAssertEqual(token.observer.observedRegion.description, "a(*),source(name)")
+        
         try withExtendedLifetime(observer) {
             try dbQueue.inDatabase { db in
                 try db.execute(sql: "INSERT INTO a VALUES (1)") // 1
@@ -44,10 +47,13 @@ class ValueObservationRegionRecordingTests: GRDBTestCase {
             
             waitForExpectations(timeout: 1, handler: nil)
             XCTAssertEqual(results, [0, 1, 2, 3])
+            
+            let token = observer as! ValueObserverToken<ValueReducers.Fetch<Int>> // Non-public implementation detail
+            XCTAssertEqual(token.observer.observedRegion.description, "b(*),source(name)")
         }
     }
     
-    func testRegionRecordingWithoutInitialFetch() throws {
+    func testAsyncSchedulingWithoutInitialFetch() throws {
         let dbQueue = try makeDatabaseQueue()
         try dbQueue.write {
             try $0.execute(sql: """
@@ -74,6 +80,8 @@ class ValueObservationRegionRecordingTests: GRDBTestCase {
             notificationExpectation.fulfill()
         }
         
+        // Can't test observedRegion because it is defined asynchronously
+        
         try withExtendedLifetime(observer) {
             try dbQueue.inDatabase { db in
                 try db.execute(sql: "INSERT INTO a VALUES (1)") // 1
@@ -86,6 +94,151 @@ class ValueObservationRegionRecordingTests: GRDBTestCase {
             
             waitForExpectations(timeout: 1, handler: nil)
             XCTAssertEqual(results, [1, 2, 3])
+            
+            let token = observer as! ValueObserverToken<ValueReducers.Fetch<Int>> // Non-public implementation detail
+            XCTAssertEqual(token.observer.observedRegion.description, "b(*),source(name)")
+        }
+    }
+    
+    func testAsyncSchedulingWithInitialFetch() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.write {
+            try $0.execute(sql: """
+                CREATE TABLE source(name TEXT);
+                INSERT INTO source VALUES ('a');
+                CREATE TABLE a(value INTEGER);
+                CREATE TABLE b(value INTEGER);
+                """)
+        }
+        
+        var results: [Int] = []
+        let notificationExpectation = expectation(description: "notification")
+        notificationExpectation.assertForOverFulfill = true
+        notificationExpectation.expectedFulfillmentCount = 4
+        
+        var observation = ValueObservation.tracking { db -> Int in
+            let table = try String.fetchOne(db, sql: "SELECT name FROM source")!
+            return try Int.fetchOne(db, sql: "SELECT COUNT(*) FROM \(table)")!
+        }
+        observation.scheduling = .async(onQueue: DispatchQueue.main, startImmediately: true)
+        
+        let observer = try observation.start(in: dbQueue) { count in
+            results.append(count)
+            notificationExpectation.fulfill()
+        }
+        
+        // Can't test observedRegion because it is defined asynchronously
+        
+        try withExtendedLifetime(observer) {
+            try dbQueue.inDatabase { db in
+                try db.execute(sql: "INSERT INTO a VALUES (1)") // 1
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // -
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // -
+                try db.execute(sql: "UPDATE source SET name = 'b'") // 2
+                try db.execute(sql: "INSERT INTO a VALUES (1)") // -
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // 3
+            }
+            
+            waitForExpectations(timeout: 1, handler: nil)
+            XCTAssertEqual(results, [0, 1, 2, 3])
+            
+            let token = observer as! ValueObserverToken<ValueReducers.Fetch<Int>> // Non-public implementation detail
+            XCTAssertEqual(token.observer.observedRegion.description, "b(*),source(name)")
+        }
+    }
+    
+    func testUnsafeSchedulingWithoutInitialFetch() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.write {
+            try $0.execute(sql: """
+                CREATE TABLE source(name TEXT);
+                INSERT INTO source VALUES ('a');
+                CREATE TABLE a(value INTEGER);
+                CREATE TABLE b(value INTEGER);
+                """)
+        }
+        
+        var results: [Int] = []
+        let notificationExpectation = expectation(description: "notification")
+        notificationExpectation.assertForOverFulfill = true
+        notificationExpectation.expectedFulfillmentCount = 3
+        
+        var observation = ValueObservation.tracking { db -> Int in
+            let table = try String.fetchOne(db, sql: "SELECT name FROM source")!
+            return try Int.fetchOne(db, sql: "SELECT COUNT(*) FROM \(table)")!
+        }
+        observation.scheduling = .unsafe(startImmediately: false)
+        
+        let observer = try observation.start(in: dbQueue) { count in
+            results.append(count)
+            notificationExpectation.fulfill()
+        }
+        
+        // Can't test observedRegion because it is defined asynchronously
+        
+        try withExtendedLifetime(observer) {
+            try dbQueue.inDatabase { db in
+                try db.execute(sql: "INSERT INTO a VALUES (1)") // 1
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // -
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // -
+                try db.execute(sql: "UPDATE source SET name = 'b'") // 2
+                try db.execute(sql: "INSERT INTO a VALUES (1)") // -
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // 3
+            }
+            
+            waitForExpectations(timeout: 1, handler: nil)
+            XCTAssertEqual(results, [1, 2, 3])
+            
+            let token = observer as! ValueObserverToken<ValueReducers.Fetch<Int>> // Non-public implementation detail
+            XCTAssertEqual(token.observer.observedRegion.description, "b(*),source(name)")
+        }
+    }
+    
+    func testUnsafeSchedulingWithInitialFetch() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.write {
+            try $0.execute(sql: """
+                CREATE TABLE source(name TEXT);
+                INSERT INTO source VALUES ('a');
+                CREATE TABLE a(value INTEGER);
+                CREATE TABLE b(value INTEGER);
+                """)
+        }
+        
+        var results: [Int] = []
+        let notificationExpectation = expectation(description: "notification")
+        notificationExpectation.assertForOverFulfill = true
+        notificationExpectation.expectedFulfillmentCount = 4
+        
+        var observation = ValueObservation.tracking { db -> Int in
+            let table = try String.fetchOne(db, sql: "SELECT name FROM source")!
+            return try Int.fetchOne(db, sql: "SELECT COUNT(*) FROM \(table)")!
+        }
+        observation.scheduling = .unsafe(startImmediately: true)
+        
+        let observer = try observation.start(in: dbQueue) { count in
+            results.append(count)
+            notificationExpectation.fulfill()
+        }
+        
+        let token = observer as! ValueObserverToken<ValueReducers.Fetch<Int>> // Non-public implementation detail
+        XCTAssertEqual(token.observer.observedRegion.description, "a(*),source(name)")
+        
+        try withExtendedLifetime(observer) {
+            try dbQueue.inDatabase { db in
+                try db.execute(sql: "INSERT INTO a VALUES (1)") // 1
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // -
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // -
+                try db.execute(sql: "UPDATE source SET name = 'b'") // 2
+                try db.execute(sql: "INSERT INTO a VALUES (1)") // -
+                try db.execute(sql: "INSERT INTO b VALUES (1)") // 3
+            }
+            
+            waitForExpectations(timeout: 1, handler: nil)
+            XCTAssertEqual(results, [0, 1, 2, 3])
+            
+            let token = observer as! ValueObserverToken<ValueReducers.Fetch<Int>> // Non-public implementation detail
+            XCTAssertEqual(token.observer.observedRegion.description, "b(*),source(name)")
         }
     }
 }
