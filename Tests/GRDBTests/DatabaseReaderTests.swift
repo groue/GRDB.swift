0f764059ee83a60841f6553c935ad7edf0d2a130
Gwendal Roué <gr@pierlis.com>
2019-09-30 20:10:07 +0200

    More DatabaseReaderTests

--- Tests/GRDBTests/DatabaseReaderTests.swift
+++ Tests/GRDBTests/DatabaseReaderTests.swift
@@ -1,17 +1,47 @@
 import XCTest
 #if GRDBCUSTOMSQLITE
-    import GRDBCustomSQLite
+import GRDBCustomSQLite
 #else
-    import GRDB
+import GRDB
 #endif
 
 class DatabaseReaderTests : GRDBTestCase {
     
+    func testAnyDatabaseReader() {
+        // This test passes if this code compiles.
+        let reader: DatabaseReader = DatabaseQueue()
+        let _: DatabaseReader = AnyDatabaseReader(reader)
+    }
+    
+    // MARK: - Read
+    
+    func testReadCanRead() throws {
+        func setup<T: DatabaseWriter>(_ dbWriter: T) throws -> T {
+            try dbWriter.write { db in
+                try db.execute(sql: "CREATE TABLE t (id INTEGER PRIMARY KEY)")
+            }
+            return dbWriter
+        }
+        func test(_ dbReader: DatabaseReader) throws {
+            let count = try dbReader.read { db in
+                try Int.fetchOne(db, sql: "SELECT COUNT(*) FROM t")
+            }
+            XCTAssertEqual(count, 0)
+        }
+        
+        try test(setup(makeDatabaseQueue()))
+        try test(setup(makeDatabasePool()))
+        try test(setup(makeDatabasePool()).makeSnapshot())
+        #if SQLITE_ENABLE_SNAPSHOT
+        try test(setup(makeDatabasePool()).makeSharedSnapshot())
+        #endif
+    }
+    
     func testReadPreventsDatabaseModification() throws {
         func test(_ dbReader: DatabaseReader) throws {
             do {
-                try dbReader.read {
-                    try $0.execute(sql: "CREATE TABLE t (id INTEGER PRIMARY KEY)")
+                try dbReader.read { db in
+                    try db.execute(sql: "CREATE TABLE t (id INTEGER PRIMARY KEY)")
                 }
                 XCTFail("Expected error")
             } catch let error as DatabaseError where error.resultCode == .SQLITE_READONLY {
@@ -25,8 +55,56 @@ class DatabaseReaderTests : GRDBTestCase {
         try test(makeDatabasePool().makeSharedSnapshot())
         #endif
     }
-
-    func testUnsafeReentrantRead() throws {
+    
+    // MARK: - UnsafeRead
+    
+    func testUnsafeReadCanRead() throws {
+        func setup<T: DatabaseWriter>(_ dbWriter: T) throws -> T {
+            try dbWriter.write { db in
+                try db.execute(sql: "CREATE TABLE t (id INTEGER PRIMARY KEY)")
+            }
+            return dbWriter
+        }
+        func test(_ dbReader: DatabaseReader) throws {
+            let count = try dbReader.unsafeRead { db in
+                try Int.fetchOne(db, sql: "SELECT COUNT(*) FROM t")
+            }
+            XCTAssertEqual(count, 0)
+        }
+        
+        try test(setup(makeDatabaseQueue()))
+        try test(setup(makeDatabasePool()))
+        try test(setup(makeDatabasePool()).makeSnapshot())
+        #if SQLITE_ENABLE_SNAPSHOT
+        try test(setup(makeDatabasePool()).makeSharedSnapshot())
+        #endif
+    }
+    
+    // MARK: - UnsafeReentrantRead
+    
+    func testUnsafeReentrantReadCanRead() throws {
+        func setup<T: DatabaseWriter>(_ dbWriter: T) throws -> T {
+            try dbWriter.write { db in
+                try db.execute(sql: "CREATE TABLE t (id INTEGER PRIMARY KEY)")
+            }
+            return dbWriter
+        }
+        func test(_ dbReader: DatabaseReader) throws {
+            let count = try dbReader.unsafeReentrantRead { db in
+                try Int.fetchOne(db, sql: "SELECT COUNT(*) FROM t")
+            }
+            XCTAssertEqual(count, 0)
+        }
+        
+        try test(setup(makeDatabaseQueue()))
+        try test(setup(makeDatabasePool()))
+        try test(setup(makeDatabasePool()).makeSnapshot())
+        #if SQLITE_ENABLE_SNAPSHOT
+        try test(setup(makeDatabasePool()).makeSharedSnapshot())
+        #endif
+    }
+    
+    func testUnsafeReentrantReadIsReentrant() throws {
         func test(_ dbReader: DatabaseReader) throws {
             try dbReader.unsafeReentrantRead { db1 in
                 try dbReader.unsafeReentrantRead { db2 in
@@ -46,11 +124,7 @@ class DatabaseReaderTests : GRDBTestCase {
         #endif
     }
     
-    func testAnyDatabaseReader() {
-        // This test passes if this code compiles.
-        let reader: DatabaseReader = DatabaseQueue()
-        let _: DatabaseReader = AnyDatabaseReader(reader)
-    }
+    // MARK: - AsyncRead
     
     #if compiler(>=5.0)
     func testAsyncRead() throws {
@@ -113,4 +187,70 @@ class DatabaseReaderTests : GRDBTestCase {
         #endif
     }
     #endif
+    
+    // MARK: - Function
+    
+    func testAddFunction() throws {
+        func test(_ dbReader: DatabaseReader) throws {
+            let f = DatabaseFunction("f", argumentCount: 0, pure: true) { _ in 0 }
+            dbReader.add(function: f)
+            let value = try dbReader.read { db in
+                try Int.fetchOne(db, sql: "SELECT f()")
+            }
+            XCTAssertEqual(value, 0)
+        }
+        
+        try test(makeDatabaseQueue())
+        try test(makeDatabasePool())
+        try test(makeDatabasePool().makeSnapshot())
+        #if SQLITE_ENABLE_SNAPSHOT
+        try test(makeDatabasePool().makeSharedSnapshot())
+        #endif
+    }
+    
+    // MARK: - Collation
+    
+    func testAddCollation() throws {
+        func test(_ dbReader: DatabaseReader) throws {
+            let collation = DatabaseCollation("c") { _, _ in .orderedSame }
+            dbReader.add(collation: collation)
+            let value = try dbReader.read { db in
+                try Int.fetchOne(db, sql: "SELECT 'foo' AS str ORDER BY str COLLATE c")
+            }
+            XCTAssertEqual(value, 0)
+        }
+        
+        try test(makeDatabaseQueue())
+        try test(makeDatabasePool())
+        try test(makeDatabasePool().makeSnapshot())
+        #if SQLITE_ENABLE_SNAPSHOT
+        try test(makeDatabasePool().makeSharedSnapshot())
+        #endif
+    }
+    
+    // MARK: - Backup
+    
+    func testBackup() throws {
+        func setup<T: DatabaseWriter>(_ dbWriter: T) throws -> T {
+            try dbWriter.write { db in
+                try db.execute(sql: "CREATE TABLE t (id INTEGER PRIMARY KEY)")
+            }
+            return dbWriter
+        }
+        func test(_ dbReader: DatabaseReader) throws {
+            let dbQueue = try makeDatabaseQueue()
+            try dbReader.backup(to: dbQueue)
+            let count = try dbReader.read { db in
+                try Int.fetchOne(db, sql: "SELECT COUNT(*) FROM t")
+            }
+            XCTAssertEqual(count, 0)
+        }
+        
+        try test(setup(makeDatabaseQueue()))
+        try test(setup(makeDatabasePool()))
+        try test(setup(makeDatabasePool()).makeSnapshot())
+        #if SQLITE_ENABLE_SNAPSHOT
+        try test(setup(makeDatabasePool()).makeSharedSnapshot())
+        #endif
+    }
 }
