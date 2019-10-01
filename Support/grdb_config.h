21968abdc829cd7b0d7fc0c95d56281e2c3f743e
Gwendal Roué <gr@pierlis.com>
2019-10-01 19:58:23 +0200

    Test enabling SQLITE_ENABLE_SNAPSHOT and SQLITE_ENABLE_PREUPDATE_HOOK via CocoaPods
    See #614

--- Support/grdb_config.h
+++ Support/grdb_config.h
@@ -5,12 +5,10 @@
     #if defined(GRDBCIPHER)
         #include <SQLCipher/sqlite3.h>
     #elif !defined(GRDBCUSTOMSQLITE)
-        #define GRDB_NEEDS_MISSING_APIS
         #include <sqlite3.h>
     #endif
 #else
     #if !defined(GRDBCUSTOMSQLITE)
-        #define GRDB_NEEDS_MISSING_APIS
         #include <sqlite3.h>
     #endif
 #endif
@@ -25,6 +23,8 @@ static inline void registerErrorLogCallback(errorLogCallback callback) {
 
 // Expose APIs that are missing from system <sqlite3.h>
 #ifdef GRDB_NEEDS_MISSING_APIS
+
+#ifdef SQLITE_ENABLE_SNAPSHOT
 typedef struct sqlite3_snapshot {
   unsigned char hidden[48];
 } sqlite3_snapshot;
@@ -44,6 +44,28 @@ SQLITE_API SQLITE_EXPERIMENTAL int sqlite3_snapshot_cmp(
   sqlite3_snapshot *p2
 );
 SQLITE_API SQLITE_EXPERIMENTAL int sqlite3_snapshot_recover(sqlite3 *db, const char *zDb);
-#endif
+#endif /* SQLITE_ENABLE_SNAPSHOT */
+
+#ifdef SQLITE_ENABLE_PREUPDATE_HOOK
+SQLITE_API void *sqlite3_preupdate_hook(
+  sqlite3 *db,
+  void(*xPreUpdate)(
+    void *pCtx,                   /* Copy of third arg to preupdate_hook() */
+    sqlite3 *db,                  /* Database handle */
+    int op,                       /* SQLITE_UPDATE, DELETE or INSERT */
+    char const *zDb,              /* Database name */
+    char const *zName,            /* Table name */
+    sqlite3_int64 iKey1,          /* Rowid of row about to be deleted/updated */
+    sqlite3_int64 iKey2           /* New rowid value (for a rowid UPDATE) */
+  ),
+  void*
+);
+SQLITE_API int sqlite3_preupdate_old(sqlite3 *, int, sqlite3_value **);
+SQLITE_API int sqlite3_preupdate_count(sqlite3 *);
+SQLITE_API int sqlite3_preupdate_depth(sqlite3 *);
+SQLITE_API int sqlite3_preupdate_new(sqlite3 *, int, sqlite3_value **);
+#endif /* SQLITE_ENABLE_PREUPDATE_HOOK */
+
+#endif /* GRDB_NEEDS_MISSING_APIS */
 
 #endif /* grdb_config_h */
---
+++ Tests/CocoaPods/GRDBiOS-static/Extra.swift
@@ -0,0 +1,13 @@
+import GRDB
+
+func test_SQLITE_ENABLE_FTS5() {
+    _ = FTS5.self
+}
+
+func test_SQLITE_ENABLE_SNAPSHOT() {
+    _ = DatabaseHistoricalSnapshot.self
+}
+
+func test_SQLITE_ENABLE_PREUPDATE_HOOK() {
+    _ = DatabasePreUpdateEvent.self
+}
