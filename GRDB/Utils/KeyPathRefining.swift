9dcd0b8f429ff37b6eaebf8247ca621f00ea3fa5
Gwendal Roué <gr@pierlis.com>
2020-01-24 07:45:20 +0100

    Fix KeyPathRefining for compiler(<5.1)

--- GRDB/Utils/KeyPathRefining.swift
+++ GRDB/Utils/KeyPathRefining.swift
@@ -20,6 +20,15 @@ extension KeyPathRefining {
         return result
     }
     
+    #if compiler(<5.1)
+    @inlinable
+    func with<T>(_ keyPath: WritableKeyPath<Self, T?>, _ value: T) -> Self {
+        var result = self
+        result[keyPath: keyPath] = value
+        return result
+    }
+    #endif
+    
     @inlinable
     func map<T>(_ keyPath: WritableKeyPath<Self, T>, _ transform: (T) throws -> T) rethrows -> Self {
         var result = self
