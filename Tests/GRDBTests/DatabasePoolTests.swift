a3cc5745ac60a7ef69211d8cfe16310d6b80c15a
Gwendal Roué <gr@pierlis.com>
2020-05-10 15:31:56 +0200

    Revert "Test for the immutable flag"
    This reverts commit 8a595be54f84b0207ccd15a106b6bc0fbf8be7b0.

--- Tests/GRDBTests/DatabasePoolTests.swift
+++ Tests/GRDBTests/DatabasePoolTests.swift
@@ -14,8 +14,7 @@ class DatabasePoolTests: GRDBTestCase {
     func testPersistentWALModeEnabled() throws {
         let path: String
         do {
-            var configuration = dbConfiguration!
-            configuration.prepareDatabase = { db in
+            dbConfiguration.prepareDatabase = { db in
                 var flag: CInt = 1
                 let code = withUnsafeMutablePointer(to: &flag) { flagP in
                     sqlite3_file_control(db.sqliteConnection, nil, SQLITE_FCNTL_PERSIST_WAL, flagP)
@@ -24,7 +23,7 @@ class DatabasePoolTests: GRDBTestCase {
                     throw DatabaseError(resultCode: ResultCode(rawValue: code))
                 }
             }
-            let dbPool = try makeDatabasePool(configuration: configuration)
+            let dbPool = try makeDatabasePool()
             path = dbPool.path
         }
         let fm = FileManager()
@@ -36,8 +35,7 @@ class DatabasePoolTests: GRDBTestCase {
     func testPersistentWALModeDisabled() throws {
         let path: String
         do {
-            var configuration = dbConfiguration!
-            configuration.prepareDatabase = { db in
+            dbConfiguration.prepareDatabase = { db in
                 var flag: CInt = 0
                 let code = withUnsafeMutablePointer(to: &flag) { flagP in
                     sqlite3_file_control(db.sqliteConnection, nil, SQLITE_FCNTL_PERSIST_WAL, flagP)
@@ -46,55 +44,12 @@ class DatabasePoolTests: GRDBTestCase {
                     throw DatabaseError(resultCode: ResultCode(rawValue: code))
                 }
             }
-            let dbPool = try makeDatabasePool(configuration: configuration)
+            let dbPool = try makeDatabasePool()
             path = dbPool.path
         }
         let fm = FileManager()
         XCTAssertTrue(fm.fileExists(atPath: path))
         XCTAssertFalse(fm.fileExists(atPath: path + "-wal"))
         XCTAssertFalse(fm.fileExists(atPath: path + "-shm"))
-    }
-    
-    func testReadonlyAccessWithPersistentWALModeDisabled() throws {
-        // Create a WAL database without  -shm and -wal temporary files:
-        let path: String
-        do {
-            var configuration = dbConfiguration!
-            configuration.prepareDatabase = { db in
-                var flag: CInt = 0
-                let code = withUnsafeMutablePointer(to: &flag) { flagP in
-                    sqlite3_file_control(db.sqliteConnection, nil, SQLITE_FCNTL_PERSIST_WAL, flagP)
-                }
-                guard code == SQLITE_OK else {
-                    throw DatabaseError(resultCode: ResultCode(rawValue: code))
-                }
-            }
-            let dbPool = try makeDatabasePool(configuration: configuration)
-            path = dbPool.path
-        }
-        
-        // CAN NOT open readonly connection to WAL database without -shm
-        // and -wal temporary files:
-        // https://www.sqlite.org/walformat.html#operations_that_require_locks_and_which_locks_those_operations_use
-        do {
-            var configuration = dbConfiguration!
-            configuration.readonly = true
-            _ = try DatabaseQueue(path: path, configuration: configuration)
-            XCTFail("Expected error")
-        } catch DatabaseError.SQLITE_CANTOPEN {
-        }
-        
-        // CAN open readonly connection to WAL database without -shm
-        // and -wal temporary files, if database is declared as immutable:
-        // https://www.sqlite.org/walformat.html#operations_that_require_locks_and_which_locks_those_operations_use
-        do {
-            var urlComponents = URLComponents(url: URL(fileURLWithPath: path), resolvingAgainstBaseURL: false)!
-            urlComponents.queryItems = [URLQueryItem(name: "immutable", value: "1")]
-            let immutableURL = urlComponents.url!
-            
-            var configuration = dbConfiguration!
-            configuration.readonly = true
-            _ = try DatabaseQueue(path: immutableURL.absoluteString, configuration: configuration)
-        }
     }
 }
