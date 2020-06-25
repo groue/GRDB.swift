22bb800f2e78e361a0b52c9356eaa38b3bd6aa84
Gwendal Roué <gr@pierlis.com>
2020-06-25 08:13:59 +0200

    Embed CombineExpectations

---
+++ Tests/CombineExpectations/PublisherExpectations/Inverted.swift
@@ -0,0 +1,29 @@
+import XCTest
+
+extension PublisherExpectations {
+    /// A publisher expectation that fails if the base expectation is fulfilled.
+    ///
+    /// When waiting for this expectation, you receive the same result and
+    /// eventual error as the base expectation.
+    ///
+    /// For example:
+    ///
+    ///     // SUCCESS: no timeout, no error
+    ///     func testPassthroughSubjectDoesNotFinish() throws {
+    ///         let publisher = PassthroughSubject<String, Never>()
+    ///         let recorder = publisher.record()
+    ///         try wait(for: recorder.finished.inverted, timeout: 1)
+    ///     }
+    public struct Inverted<Base: PublisherExpectation>: PublisherExpectation {
+        let base: Base
+        
+        public func _setup(_ expectation: XCTestExpectation) {
+            base._setup(expectation)
+            expectation.isInverted.toggle()
+        }
+        
+        public func get() throws -> Base.Output {
+            try base.get()
+        }
+    }
+}
