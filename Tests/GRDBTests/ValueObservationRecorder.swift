606cc2cb37947c9b98a0eb621181edf06cbf0151
Gwendal Roué <gr@pierlis.com>
2020-07-14 18:25:31 +0200

    Give more time to SQLCipher4 Travis tests

--- Tests/GRDBTests/ValueObservationRecorder.swift
+++ Tests/GRDBTests/ValueObservationRecorder.swift
@@ -424,7 +424,7 @@ extension GRDBTestCase {
     {
         #if SQLITE_HAS_CODEC || GRDBCUSTOMSQLITE
         // debug SQLite builds can be *very* slow
-        let timeout: TimeInterval = 3
+        let timeout: TimeInterval = 4
         #else
         let timeout: TimeInterval = 1
         #endif
