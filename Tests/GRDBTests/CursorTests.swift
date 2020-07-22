62f33e1d5541afc5e22961f599aeee2edd980a1c
Gwendal Roué <gr@pierlis.com>
2020-07-22 08:39:45 +0200

    Dictionary(grouping:by:) with Cursor

--- Tests/GRDBTests/CursorTests.swift
+++ Tests/GRDBTests/CursorTests.swift
@@ -170,4 +170,22 @@ class CursorTests: GRDBTestCase {
             XCTAssertTrue(suffix == [1, 2, 3, 4, 5])
         }
     }
+    
+    func testDictionaryGrouping() throws {
+        do {
+            let cursor = AnyCursor<Int>([])
+            let dictionary = try Dictionary(grouping: cursor, by: { $0 % 2 })
+            XCTAssertEqual(dictionary, [:])
+        }
+        do {
+            let cursor = AnyCursor([1, 2, 3, 4, 5])
+            let dictionary = try Dictionary(grouping: cursor, by: { $0 % 2 })
+            XCTAssertEqual(dictionary, [0: [2, 4], 1: [1, 3, 5]])
+        }
+        do {
+            let cursor = AnyCursor([5, 4, 3, 2, 1])
+            let dictionary = try Dictionary(grouping: cursor, by: { $0 % 2 })
+            XCTAssertEqual(dictionary, [0: [4, 2], 1: [5, 3, 1]])
+        }
+    }
 }
