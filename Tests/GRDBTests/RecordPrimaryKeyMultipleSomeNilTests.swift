bd53c830f2a6b90ebd4b20a3e39c31080b60f30f
Thomas Counsell <tom@eyfs.info>
2021-08-05 14:13:13 +0100

    Error updating some records with multi-column primary keys
    If a record has
    * a multi-column primary key AND
    * one of those columns is nil
    
    then updating the record will fail.
    
    It seems to be generating statements like:
    
        UPDATE .... WHERE key1 = "thing" AND key2 = NULL
    
    Where I was expecting it to generate statements like:
    
        UPDATE .... WHERE key1 = "thing" AND key2 IS NULL

---
+++ Tests/GRDBTests/RecordPrimaryKeyMultipleSomeNilTests.swift
@@ -0,0 +1,78 @@
+import XCTest
+import GRDB
+
+private enum MaybeRemoteMaybeLocalIdError: Error {
+    case mustHaveLocalIDorRemoteID
+}
+
+// MaybeRemoteMaybeLocalID is a record that might have a local id, or might have a remote id, or possibly both.
+// So its primary key is a combination of the local and remote ids,
+// permitting one or the other (but not both) to be null
+private struct MaybeRemoteMaybeLocalID : Codable, MutablePersistableRecord, FetchableRecord {
+    let localID: UInt64?
+    let remoteID: UInt64?
+    var thing: String
+    
+    init(localID: UInt64? = nil, remoteID: UInt64? = nil, thing: String) throws {
+        guard localID != nil || remoteID != nil else { throw MaybeRemoteMaybeLocalIdError.mustHaveLocalIDorRemoteID }
+        self.localID = localID
+        self.remoteID = remoteID
+        self.thing = thing
+    }
+    
+    static func setup(inDatabase db: Database) throws {
+        try db.execute(sql: """
+            CREATE TABLE maybeRemoteMaybeLocalID (
+                localID INT UNIQUE,
+                remoteID INT UNIQUE,
+                thing TEXT NOT NULL,
+                PRIMARY KEY (localID, remoteID),
+                CONSTRAINT check_primary CHECK (localID IS NOT NULL OR remoteID IS NOT NULL)
+            )
+            """)
+    }
+}
+
+
+class RecordPrimaryKeyMultipleSomeNilTests: GRDBTestCase {
+    
+    override func setup(_ dbWriter: DatabaseWriter) throws {
+        var migrator = DatabaseMigrator()
+        migrator.registerMigration("createMaybeRemoteMaybeLocalID", migrate: MaybeRemoteMaybeLocalID.setup)
+        try migrator.migrate(dbWriter)
+    }
+    
+    
+    // MARK: - Insert
+    
+    // This works fine
+    func testInsert() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.inDatabase { db in
+            var record = try MaybeRemoteMaybeLocalID(localID: 1, thing: "Local One")
+            try record.insert(db)
+            
+            let row = try Row.fetchOne(db, sql: "SELECT * FROM maybeRemoteMaybeLocalID WHERE localID = ?", arguments: [record.localID])!
+            assert(record, isEncodedIn: row)
+        }
+    }
+    
+    // MARK: - Update
+    
+    /// This fails, because it generates the sql something like:
+    /// `UPDATE ... WHERE "localID"=1 AND "remoteID"=NULL`
+    /// where I was expecting it to be generating something like:
+    ///  `UPDATE ... WHERE "localID"=1 AND "remoteID" IS NULL`
+    func testUpdate() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.inDatabase { db in
+            var record = try MaybeRemoteMaybeLocalID(localID: 1, thing: "Local One")
+            try record.insert(db)
+            record.thing = "Local One Updated"
+            try record.update(db)
+            
+            let row = try Row.fetchOne(db, sql: "SELECT * FROM maybeRemoteMaybeLocalID WHERE localID = ?", arguments: [record.localID])!
+            assert(record, isEncodedIn: row)
+        }
+    }
+}
