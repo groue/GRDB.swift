2f3311cd1e5911429b24c43f9b304362ddb76e00
Ethan Wong <e1hanW0ng@gmail.com>
2021-08-22 16:12:39 +0800

    Add match operator in ColumnExpression for FTS5

--- GRDB/QueryInterface/FTS5+QueryInterface.swift
+++ GRDB/QueryInterface/FTS5+QueryInterface.swift
@@ -41,4 +41,18 @@ extension TableRecord {
         all().matching(pattern)
     }
 }
+
+extension ColumnExpression {
+    
+    /// A matching SQL expression with the `MATCH` SQL operator.
+    ///
+    ///     // content MATCH '...'
+    ///     Column("content").match(pattern)
+    ///
+    /// If the search pattern is nil, SQLite will evaluate the expression
+    /// to false.
+    public func match(_ pattern: FTS5Pattern?) -> SQLExpression {
+        .binary(.match, sqlExpression, pattern?.sqlExpression ?? .null)
+    }
+}
 #endif
