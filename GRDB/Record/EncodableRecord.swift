06763e47d2e57dec4719f216aa2825b11845fcbf
Gwendal Roué <gr@pierlis.com>
2021-08-28 16:05:41 +0200

    Share convertToSnakeCase()

--- GRDB/Record/EncodableRecord.swift
+++ GRDB/Record/EncodableRecord.swift
@@ -428,65 +428,9 @@ public enum DatabaseKeyEncodingStrategy {
         case .useDefaultKeys:
             return key.stringValue
         case .convertToSnakeCase:
-            return Self.convertToSnakeCase(key.stringValue)
+            return key.stringValue.convertToSnakeCase()
         case let .custom(column):
             return column(key)
         }
-    }
-    
-    /// This function is taken directly from Apple's open source JSONEncoder.
-    /// https://github.com/apple/swift-corelibs-foundation/blob/eec4b26deee34edb7664ddd9c1222492a399d122/Sources/Foundation/JSONEncoder.swift#L127
-    private static func convertToSnakeCase(_ stringKey: String) -> String {
-        guard !stringKey.isEmpty else { return stringKey }
-        
-        var words: [Range<String.Index>] = []
-        // The general idea of this algorithm is to split words on transition from lower to upper case, then on
-        // transition of >1 upper case characters to lowercase
-        //
-        // myProperty -> my_property
-        // myURLProperty -> my_url_property
-        //
-        // We assume, per Swift naming conventions, that the first character of the key is lowercase.
-        var wordStart = stringKey.startIndex
-        var searchRange = stringKey.index(after: wordStart)..<stringKey.endIndex
-        
-        // Find next uppercase character
-        while let upperCaseRange = stringKey.rangeOfCharacter(from: CharacterSet.uppercaseLetters, options: [],
-                                                              range: searchRange) {
-            let untilUpperCase = wordStart..<upperCaseRange.lowerBound
-            words.append(untilUpperCase)
-            
-            // Find next lowercase character
-            searchRange = upperCaseRange.lowerBound..<searchRange.upperBound
-            guard let lowerCaseRange = stringKey.rangeOfCharacter(from: CharacterSet.lowercaseLetters, options: [],
-                                                                  range: searchRange) else {
-                // There are no more lower case letters. Just end here.
-                wordStart = searchRange.lowerBound
-                break
-            }
-            
-            // Is the next lowercase letter more than 1 after the uppercase? If so, we encountered a group of
-            // uppercase letters that we should treat as its own word
-            let nextCharacterAfterCapital = stringKey.index(after: upperCaseRange.lowerBound)
-            if lowerCaseRange.lowerBound == nextCharacterAfterCapital {
-                // The next character after capital is a lower case character and therefore not a word boundary.
-                // Continue searching for the next upper case for the boundary.
-                wordStart = upperCaseRange.lowerBound
-            } else {
-                // There was a range of >1 capital letters. Turn those into a word, stopping at the capital before
-                // the lower case character.
-                let beforeLowerIndex = stringKey.index(before: lowerCaseRange.lowerBound)
-                words.append(upperCaseRange.lowerBound..<beforeLowerIndex)
-                
-                // Next word starts at the capital before the lowercase we just found
-                wordStart = beforeLowerIndex
-            }
-            searchRange = lowerCaseRange.upperBound..<searchRange.upperBound
-        }
-        words.append(wordStart..<searchRange.upperBound)
-        let result = words.map({ (range) in
-            return stringKey[range].lowercased()
-        }).joined(separator: "_")
-        return result
     }
 }
--- GRDB/Utils/Inflections.swift
+++ GRDB/Utils/Inflections.swift
@@ -34,6 +34,66 @@ extension String {
     var digitlessRadical: String {
         String(prefix(upTo: Inflections.endIndexOfDigitlessRadical(self)))
     }
+    
+    // This function is taken directly from Apple's open source JSONDecoder.
+    // https://github.com/apple/swift-corelibs-foundation/blob/eec4b26deee34edb7664ddd9c1222492a399d122/Sources/Foundation/JSONEncoder.swift#L127
+    func convertToSnakeCase() -> String {
+        guard !isEmpty else { return self }
+        
+        var words: [Range<String.Index>] = []
+        // The general idea of this algorithm is to split words on transition
+        // from lower to upper case, then on transition of >1 upper case
+        // characters to lowercase
+        //
+        // myProperty -> my_property
+        // myURLProperty -> my_url_property
+        //
+        // We assume, per Swift naming conventions, that the first character of
+        // the key is lowercase.
+        var wordStart = startIndex
+        var searchRange = index(after: wordStart)..<endIndex
+        
+        // Find next uppercase character
+        while let upperCaseRange = rangeOfCharacter(from: CharacterSet.uppercaseLetters,
+                                                    options: [], range: searchRange) {
+            let untilUpperCase = wordStart..<upperCaseRange.lowerBound
+            words.append(untilUpperCase)
+            
+            // Find next lowercase character
+            searchRange = upperCaseRange.lowerBound..<searchRange.upperBound
+            guard let lowerCaseRange = rangeOfCharacter(from: CharacterSet.lowercaseLetters,
+                                                        options: [], range: searchRange) else {
+                // There are no more lower case letters. Just end here.
+                wordStart = searchRange.lowerBound
+                break
+            }
+            
+            // Is the next lowercase letter more than 1 after the uppercase?
+            // If so, we encountered a group of uppercase letters that we should
+            // treat as its own word
+            let nextCharacterAfterCapital = index(after: upperCaseRange.lowerBound)
+            if lowerCaseRange.lowerBound == nextCharacterAfterCapital {
+                // The next character after capital is a lower case character
+                // and therefore not a word boundary.
+                // Continue searching for the next upper case for the boundary.
+                wordStart = upperCaseRange.lowerBound
+            } else {
+                // There was a range of >1 capital letters. Turn those into a
+                // word, stopping at the capital before the lower case character.
+                let beforeLowerIndex = index(before: lowerCaseRange.lowerBound)
+                words.append(upperCaseRange.lowerBound..<beforeLowerIndex)
+                
+                // Next word starts at the capital before the lowercase we just found
+                wordStart = beforeLowerIndex
+            }
+            searchRange = lowerCaseRange.upperBound..<searchRange.upperBound
+        }
+        words.append(wordStart..<searchRange.upperBound)
+        let result = words.map({ (range) in
+            return self[range].lowercased()
+        }).joined(separator: "_")
+        return result
+    }
 }
 
 /// [**Experimental**](http://github.com/groue/GRDB.swift#what-are-experimental-features)
