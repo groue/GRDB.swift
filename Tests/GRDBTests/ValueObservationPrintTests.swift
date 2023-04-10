1add9a975205a0f4eb1fbbb9756419b65980729d
Gwendal Roué <gr@pierlis.com>
2023-04-10 11:17:04 +0200

    Fix incorrect test

--- Tests/GRDBTests/ValueObservationPrintTests.swift
+++ Tests/GRDBTests/ValueObservationPrintTests.swift
@@ -436,13 +436,17 @@ class ValueObservationPrintTests: GRDBTestCase {
             onChange: { _ in expectation.fulfill() })
         withExtendedLifetime(cancellable) {
             waitForExpectations(timeout: 1, handler: nil)
-            XCTAssertEqual(logger.strings, [
-                "start",
-                "fetch",
-                "value: nil",
+            // Order of events may not be stable, because the first
+            // "value: nil" is notified concurrently (from the reduce queue)
+            // with the first "database did change" (from the writer queue).
+            // That's why we test the sorted output.
+            XCTAssertEqual(logger.strings.sorted(), [
                 "database did change",
                 "fetch",
+                "fetch",
+                "start",
                 "tracked region: \(expectedRegion)",
+                "value: nil",
                 "value: nil"])
         }
     }
