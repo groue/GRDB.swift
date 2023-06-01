fa681fb8c6135e0bdc78fa83375425833f104c5a
Gwendal Roué <gr@pierlis.com>
2023-06-01 19:07:28 +0200

    Enhance error message when WALSnapshot initializer fails

--- GRDB/Core/WALSnapshot.swift
+++ GRDB/Core/WALSnapshot.swift
@@ -30,11 +30,30 @@ final class WALSnapshot: Sendable {
             return sqlite3_snapshot_get(db.sqliteConnection, "main", $0)
         }
         guard code == SQLITE_OK else {
+            // <https://www.sqlite.org/c3ref/snapshot_get.html>
+            //
+            // > The following must be true for sqlite3_snapshot_get() to succeed. [...]
+            // >
+            // > 1. The database handle must not be in autocommit mode.
+            // > 2. Schema S of database connection D must be a WAL
+            // >    mode database.
+            // > 3. There must not be a write transaction open on schema S
+            // >    of database connection D.
+            // > 4. One or more transactions must have been written to the
+            // >    current wal file since it was created on disk (by any
+            // >    connection). This means that a snapshot cannot be taken
+            // >    on a wal mode database with no wal file immediately
+            // >    after it is first opened. At least one transaction must
+            // >    be written to it first.
+            
+            // Test condition 1:
             if sqlite3_get_autocommit(db.sqliteConnection) != 0 {
                 throw DatabaseError(resultCode: code, message: """
                     Can't create snapshot because database is in autocommit mode.
                     """)
             }
+            
+            // Test condition 2:
             if let journalMode = try? String.fetchOne(db, sql: "PRAGMA journal_mode"),
                journalMode != "wal"
             {
@@ -42,7 +61,14 @@ final class WALSnapshot: Sendable {
                     Can't create snapshot because database is not in WAL mode.
                     """)
             }
-            throw DatabaseError(resultCode: code)
+            
+            // Condition 3 can't happen because GRDB only calls this
+            // initializer from read transactions.
+            //
+            // Hence it is condition 4 that is false:
+            throw DatabaseError(resultCode: code, message: """
+                Can't create snapshot from a missing or empty wal file.
+                """)
         }
         guard let sqliteSnapshot else {
             throw DatabaseError(resultCode: .SQLITE_INTERNAL) // WTF SQLite?
