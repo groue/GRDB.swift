2f93f00b4112eb04d4d5df8b8ec16abb3882f2b6
Gwendal Roué <gr@pierlis.com>
2024-02-10 20:07:37 +0100

    DatabaseCancellable is Sendable

--- GRDB/ValueObservation/DatabaseCancellable.swift
+++ GRDB/ValueObservation/DatabaseCancellable.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 /// A protocol indicating that an activity or action supports cancellation.
 ///
 /// ## Topics
@@ -5,7 +7,7 @@
 /// ### Supporting Types
 ///
 /// - ``AnyDatabaseCancellable``
-public protocol DatabaseCancellable {
+public protocol DatabaseCancellable: Sendable {
     /// Cancel the activity.
     func cancel()
 }
@@ -15,31 +17,40 @@ public protocol DatabaseCancellable {
 ///
 /// An `AnyDatabaseCancellable` instance automatically calls ``cancel()``
 ///  when deinitialized.
-public class AnyDatabaseCancellable: DatabaseCancellable {
-    private var _cancel: (() -> Void)?
+public final class AnyDatabaseCancellable: DatabaseCancellable, @unchecked Sendable {
+    // @unchecked Sendable due to `_cancel`, which is protected by `lock`.
+    private let lock = NSLock()
+    private var _cancel: (@Sendable () -> Void)?
+    
+    var isCancelled: Bool {
+        lock.withLock { _cancel == nil }
+    }
+    
+    convenience init() {
+        self.init(cancel: { })
+    }
     
     /// Initializes the cancellable object with the given cancel-time closure.
-    public init(cancel: @escaping () -> Void) {
+    public init(cancel: @escaping @Sendable () -> Void) {
         _cancel = cancel
     }
     
     /// Creates a cancellable object that forwards cancellation to `base`.
     public convenience init(_ base: some DatabaseCancellable) {
-        var cancellable = Optional.some(base)
         self.init {
-            cancellable?.cancel()
-            cancellable = nil // Release memory
+            base.cancel()
         }
     }
     
     deinit {
-        _cancel?()
+        cancel()
     }
     
     public func cancel() {
-        // Don't prevent multiple concurrent calls to _cancel, because it is
-        // pointless. But release memory!
-        _cancel?()
+        lock.lock()
+        let cancel = _cancel
         _cancel = nil
+        lock.unlock()
+        cancel?()
     }
 }
