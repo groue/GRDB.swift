4c541a99b541caca9d8572641e5e2d98588d36ce
Gwendal Roué <gr@pierlis.com>
2024-09-14 12:23:07 +0200

    Min iOS target: v18

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import Foundation
@@ -34,7 +34,7 @@ let package = Package(
     name: "GRDB",
     defaultLocalization: "en", // for tests
     platforms: [
-        .iOS(.v11),
+        .iOS(.v18),
         .macOS(.v10_13),
         .tvOS(.v11),
         .watchOS(.v4),
