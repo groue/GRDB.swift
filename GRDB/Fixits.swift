68d12916e641d3f03e8118f510dbf36cb25dfce7
Gwendal Roué <gr@pierlis.com>
2024-10-06 14:42:49 +0200

    Fix Swiftlint warning

--- GRDB/Fixits.swift
+++ GRDB/Fixits.swift
@@ -1,4 +1,5 @@
 // Fixits for changes introduced by GRDB 7.0.0
+// swiftlint:disable all
 
 extension Configuration {
     @available(*, unavailable, message: "The default transaction kind is now automatically managed.")
@@ -15,3 +16,5 @@ extension DatabasePool {
     @available(*, unavailable, message: "concurrentRead has been removed. Use `asyncConcurrentRead` instead.")
     public func concurrentRead<T>(_ value: @escaping (Database) throws -> T) -> DatabaseFuture<T> { preconditionFailure() }
 }
+
+// swiftlint:enable all
