groue/GRDB.swift

commit c7bbebc80f864252d1b5b32d19fc282a4f4177ee

Change the way we count changes in order to support INSTEAD OF triggers
Database.changesCount calls sqlite3_changes(), whose documentation says:

> https://sqlite.org/c3ref/changes.html
> Changes to a view that are intercepted by INSTEAD OF triggers are not counted.

Since we want to support INSTEAD OF triggers well, so that record types can be based on views, we switch to sqlite3_total_changes().

GRDB/QueryInterface/Request/QueryInterfaceRequest.swift
@@ -635,8 +635,9 @@ extension QueryInterfaceRequest {
     @discardableResult
     public func deleteAll(_ db: Database) throws -> Int {
         let statement = try SQLQueryGenerator(relation: relation).makeDeleteStatement(db)
+        let prevCount = db.totalChangesCount
         try statement.execute()
-        return db.changesCount
+        return db.totalChangesCount - prevCount
     }
 }
 
@@ -1135,8 +1136,9 @@ extension QueryInterfaceRequest {
             // database not hit
             return 0
         }
+        let prevCount = db.totalChangesCount
         try updateStatement.execute()
-        return db.changesCount
+        return db.totalChangesCount - prevCount
     }
     
     /// Updates matching rows, and returns the number of updated rows.

GRDB/Record/MutablePersistableRecord+Delete.swift
@@ -53,7 +53,8 @@ extension MutablePersistableRecord {
             // Nil primary key
             return false
         }
+        let prevCount = db.totalChangesCount
         try statement.execute()
-        return db.changesCount > 0
+        return (db.totalChangesCount - prevCount) > 0
     }
 }

GRDB/Record/MutablePersistableRecord+Update.swift
@@ -956,8 +956,10 @@ extension MutablePersistableRecord {
             // Nil primary key
             try dao.recordNotFound()
         }
+        let prevCount = db.totalChangesCount
         let returned = try fetch(statement)
-        if db.changesCount == 0 {
+        let changesCount = db.totalChangesCount - prevCount
+        if changesCount == 0 {
             // No row was updated
             try dao.recordNotFound()
         }

commit 3f27fb9289687116e60efef6487f4a8d7ed13d24

Fix #1820
The regression was introduced by c7bbebc

GRDB/Core/Database.swift
@@ -2019,6 +2019,34 @@ extension Database {
 #endif
 
 extension Database {
+    /// Returns the count of changes executed by one statement execution.
+    func countChanges<T>(_ count: inout Int, forTable tableName: String, updates: () throws -> T) throws -> T {
+        // Database.changesCount calls sqlite3_changes(), whose documentation says:
+        //
+        // > https://sqlite.org/c3ref/changes.html
+        // > Changes to a view that are intercepted by INSTEAD OF triggers are not counted.
+        //
+        // We want to support INSTEAD OF triggers, so we prefer to use
+        // sqlite3_total_changes() for views.
+        //
+        // At the same time, FTS5 has sqlite3_total_changes() report changes
+        // even when database is not changed (https://github.com/groue/GRDB.swift/issues/1820)
+        //
+        // Well, let's do our best:
+        if try viewExists(tableName) {
+            let prevCount = totalChangesCount
+            let result = try updates()
+            count = totalChangesCount - prevCount
+            return result
+        } else {
+            let result = try updates()
+            count = changesCount
+            return result
+        }
+    }
+}
+
+extension Database {
     
     // MARK: - Database-Related Types
     

GRDB/QueryInterface/Request/QueryInterfaceRequest.swift
@@ -635,9 +635,11 @@ extension QueryInterfaceRequest {
     @discardableResult
     public func deleteAll(_ db: Database) throws -> Int {
         let statement = try SQLQueryGenerator(relation: relation).makeDeleteStatement(db)
-        let prevCount = db.totalChangesCount
-        try statement.execute()
-        return db.totalChangesCount - prevCount
+        var changesCount = 0
+        try db.countChanges(&changesCount, forTable: relation.source.tableName) {
+            try statement.execute()
+        }
+        return changesCount
     }
 }
 
@@ -1136,9 +1138,11 @@ extension QueryInterfaceRequest {
             // database not hit
             return 0
         }
-        let prevCount = db.totalChangesCount
-        try updateStatement.execute()
-        return db.totalChangesCount - prevCount
+        var changesCount = 0
+        try db.countChanges(&changesCount, forTable: relation.source.tableName) {
+            try updateStatement.execute()
+        }
+        return changesCount
     }
     
     /// Updates matching rows, and returns the number of updated rows.

GRDB/Record/MutablePersistableRecord+Delete.swift
@@ -53,8 +53,10 @@ extension MutablePersistableRecord {
             // Nil primary key
             return false
         }
-        let prevCount = db.totalChangesCount
-        try statement.execute()
-        return (db.totalChangesCount - prevCount) > 0
+        var changesCount = 0
+        try db.countChanges(&changesCount, forTable: type(of: self).databaseTableName) {
+            try statement.execute()
+        }
+        return changesCount > 0
     }
 }

GRDB/Record/MutablePersistableRecord+Update.swift
@@ -956,9 +956,10 @@ extension MutablePersistableRecord {
             // Nil primary key
             try dao.recordNotFound()
         }
-        let prevCount = db.totalChangesCount
-        let returned = try fetch(statement)
-        let changesCount = db.totalChangesCount - prevCount
+        var changesCount = 0
+        let returned = try db.countChanges(&changesCount, forTable: type(of: self).databaseTableName) {
+            try fetch(statement)
+        }
         if changesCount == 0 {
             // No row was updated
             try dao.recordNotFound()