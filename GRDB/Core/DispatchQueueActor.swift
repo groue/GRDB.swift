bc91f9b4769ec9c1ec92826405e7c849f83816ac
Tim De Jong <gitlab@thinkpractice.nl>
2025-10-05 09:21:24 +0200

    Fixed formatting problems I introduced

--- GRDB/Core/DispatchQueueActor.swift
+++ GRDB/Core/DispatchQueueActor.swift
@@ -5,16 +5,16 @@
 /// Inspired by <https://forums.swift.org/t/using-dispatchqueue-as-actors-serial-executor-under-linux/75260/3>
 actor DispatchQueueActor {
     private let executor: DispatchQueueExecutor
-
+    
     /// - precondition: the queue is serial, or flags contains `.barrier`.
     init(queue: DispatchQueue, flags: DispatchWorkItemFlags = []) {
         self.executor = DispatchQueueExecutor(queue: queue, flags: flags)
     }
-
+    
     nonisolated var unownedExecutor: UnownedSerialExecutor {
         executor.asUnownedSerialExecutor()
     }
-
+    
     func execute<T>(_ work: () throws -> T) rethrows -> T {
         try work()
     }
@@ -23,22 +23,22 @@ actor DispatchQueueActor {
 private final class DispatchQueueExecutor: SerialExecutor {
     private let queue: DispatchQueue
     private let flags: DispatchWorkItemFlags
-
+    
     init(queue: DispatchQueue, flags: DispatchWorkItemFlags) {
         self.queue = queue
         self.flags = flags
     }
-
+    
     func enqueue(_ job: UnownedJob) {
         queue.async(flags: flags) {
             job.runSynchronously(on: self.asUnownedSerialExecutor())
         }
     }
-
+    
     func asUnownedSerialExecutor() -> UnownedSerialExecutor {
         UnownedSerialExecutor(ordinary: self)
     }
-
+    
     func checkIsolated() {
         dispatchPrecondition(condition: .onQueue(queue))
     }
